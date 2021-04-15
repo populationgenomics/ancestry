f4b76847a1dc7ea8a7507ec25e7145e76cc73e4b
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-04-15 15:53:09 +1000

    Add files for the analysis runner. (#5)
    * Add files for the analysis runner.
    
    * Update scripts/hail_batch/README.md
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    * Update scripts/hail_batch/README.md
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    * Change file name and subset samples to 100
    
    In order to reduce processing time, I subsampled to 100 samples rather than the original 3942. The HGDP_1KG_PCALoadingsGenerate.py file name was also changed to follow the snake-case for filenames. Max-age was changed to '15h' to allow for more computational time in case the PCA function has not finished, and the gnomAD package was removed.
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

---
+++ scripts/hail_batch/README.md
@@ -0,0 +1,8 @@
+# Generate PCA loadings, eigenvalues, and scores
+This runs a Hail query script in Dataproc using Hail Batch in order to output the PCA eigenvalues, scores, and loadings from the 1KG + HGDP dataset. To run, use conda to install the analysis-runner, then execute the following command:
+
+```
+analysis-runner --dataset ancestry \
+--access-level test --output-dir "gs://cpg-ancestry-test/1kg_hgdp_pca/v0" \
+--description "PCA loadings" main.py
+```
---
+++ scripts/hail_batch/hgdp_1kg_pca_loadings_generate.py
@@ -0,0 +1,35 @@
+"""Generates loadings, scores, and eigenvalues for the HGDP + 1KG dataset"""
+
+import click
+import pandas as pd
+from hail.methods import hwe_normalized_pca
+import hail as hl
+
+GNOMAD_HGDP_1KG_MT = (
+    'gs://gcp-public-data--gnomad/release/3.1/mt/genomes/gnomad.genomes.v3.1.hgdp_1kg_subset_dense.mt'
+)
+
+@click.command()
+@click.option('--output', help='GCS output path', required=True)
+def query(output, rerun):
+    """Query script entry point."""
+
+    hl.init(default_reference='GRCh38')
+
+    eigenvalues_path = f'{output}/eigenvalues.csv'
+    scores_path = f'{output}/scores.ht'
+    loadings_path = f'{output}/loadings.ht'
+    mt = hl.read_matrix_table(GNOMAD_HGDP_1KG_MT)
+    # test on 100 samples
+    mt_head = mt.head(n=mt.count_rows(),n_cols=100)
+    eigenvalues, scores, loadings = hl.hwe_normalized_pca(mt_head.GT, compute_loadings=True, k=20)
+    # save the list of eigenvalues
+    eigenvalues_df = pd.DataFrame(eigenvalues) 
+    eigenvalues_df.to_csv(eigenvalues_path, index=False)
+    # save the scores and loadings as a hail table
+    scores.write(scores_path, overwrite=True)
+    loadings.write(loadings_path, overwrite=True)
+
+if __name__ == '__main__':
+    query()  # pylint: disable=no-value-for-parameter
+
