1f62a081229a4a58d2bcbc98d5d2e68fa12eb47c
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-04-19 15:21:03 +1000

    Create script to perform LD-pruning
    This script performs LD pruning on the HGDP + 1KG dataset in order to reduce the total variants for downstream PCA processing.

---
+++ scripts/hail_batch/ld_prune/README.md
@@ -0,0 +1,9 @@
+# Perform ld-pruning
+
+This runs a Hail query script in Dataproc using Hail Batch in order to perform ld pruning on the 1KG + HGDP dataset. To run, use conda to install the analysis-runner, then execute the following command:
+
+```sh
+analysis-runner --dataset ancestry \
+--access-level test --output-dir "gs://cpg-ancestry-temporary/1kg_hgdp_ld_pruning/v0" \
+--description "ld pruning" main.py
+```
---
+++ scripts/hail_batch/ld_prune/main.py
@@ -0,0 +1,28 @@
+"""Run hgdp_1kg_ld_prune.py using the analysis runner."""
+
+import os
+import hail as hl
+import hailtop.batch as hb
+from analysis_runner import dataproc
+
+OUTPUT = os.getenv('OUTPUT')
+assert OUTPUT
+
+hl.init(default_reference='GRCh38')
+
+service_backend = hb.ServiceBackend(
+    billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
+)
+
+batch = hb.Batch(name='ld pruning', backend=service_backend)
+
+dataproc.hail_dataproc_job(
+    batch,
+    f'hgdp_1kg_ld_prune.py --output={OUTPUT}',
+    max_age='5h',
+    num_secondary_workers=100,
+    packages=['click'],
+    job_name='ld-prune',
+)
+
+batch.run()
