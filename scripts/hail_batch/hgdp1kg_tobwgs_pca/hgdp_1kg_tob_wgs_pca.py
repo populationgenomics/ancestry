4d7daced34c5122ef2a0300861cb69f3b0b1ded7
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-04-26 20:19:17 +1000

    Change filtering order and moving liftover step to a new script
    I reordered the filtering loci step so that it happens before joining datasets, as joins are computationally expensive so this should lower the processing time. I also created a new script (available in gnomad_90k_variants_liftover), which performs a liftover of the gnomad v2.1 loadings (from hg37 to hg38). As suggested by Vlad, I also rekeyed mt_tob_wgs, so that it has both the locus and alleles as its keys (and that way, has the same keys as mt_hgdp_1kg).

--- scripts/hail_batch/hgdp1kg_tobwgs_pca/hgdp_1kg_tob_wgs_pca.py
+++ scripts/hail_batch/hgdp1kg_tobwgs_pca/hgdp_1kg_tob_wgs_pca.py
@@ -12,9 +12,7 @@ GNOMAD_HGDP_1KG_MT = (
 
 TOB_WGS = 'gs://cpg-tob-wgs-temporary/joint_vcf/v1/work/combiner/genomes.mt'
 
-GNOMAD_V2_LOADINGS = (
-    'gs://gcp-public-data--gnomad/release/2.1/pca/gnomad.r2.1.pca_loadings.ht'
-)
+GNOMAD_LIFTOVER_LOADINGS = 'gs://cpg-reference/gnomad/gnomad_loadings_90k_liftover.ht'
 
 
 @click.command()
@@ -27,20 +25,32 @@ def query(output):  # pylint: disable=too-many-locals
     # Filter the matrix tables to rows whose keys appear in both datasets
     mt_hgdp_1kg = hl.read_matrix_table(GNOMAD_HGDP_1KG_MT)
     mt_tob_wgs = hl.read_matrix_table(TOB_WGS)
-    # Remove 'allele' row key so that rows are the same (necessary step to join)
-    mt_hgdp_1kg = mt_hgdp_1kg.key_rows_by('locus')
-    mt_hgdp_1kg = mt_hgdp_1kg.drop('alleles')
+    # make keys the same between datasets
+    mt_tob_wgs = mt_tob_wgs.key_rows_by('locus', 'alleles')
     join_rows_hgdp1kg_tobwgs = mt_hgdp_1kg.semi_join_rows(mt_tob_wgs.rows())
     join_rows_tobwgs_hgdp1kg = mt_tob_wgs.semi_join_rows(mt_hgdp_1kg.rows())
 
-    # Join dataset by merging columns
+    # add gnomad liftover loading data to both matrices
+    ht_gnomad_loadings_liftover = hl.read_table(GNOMAD_LIFTOVER_LOADINGS)
+    hgdp1kg_select_variants = join_rows_hgdp1kg_tobwgs.annotate_rows(
+        rows_to_keep=ht_gnomad_loadings_liftover[join_rows_hgdp1kg_tobwgs.locus]
+    )
+    hgdp1kg_filt = hgdp1kg_select_variants.filter_rows(
+        hgdp1kg_select_variants.locus == hgdp1kg_select_variants.rows_to_keep.liftover
+    )
+    tobwgs_select_variants = join_rows_tobwgs_hgdp1kg.annotate_rows(
+        rows_to_keep=ht_gnomad_loadings_liftover[join_rows_tobwgs_hgdp1kg.locus]
+    )
+    tobwgs_filt = tobwgs_select_variants.filter_rows(
+        tobwgs_select_variants.locus == tobwgs_select_variants.rows_to_keep.liftover
+    )
+
+    # Join datasets by merging columns
     # Entries and columns must be identical
-    change_lgt_to_gt = join_rows_tobwgs_hgdp1kg.annotate_entries(
-        GT=lgt_to_gt(join_rows_tobwgs_hgdp1kg.LGT, join_rows_tobwgs_hgdp1kg.LA)
+    change_lgt_to_gt = tobwgs_filt.annotate_entries(
+        GT=lgt_to_gt(tobwgs_filt.LGT, tobwgs_filt.LA)
     )
-    select_entries_hgdp1kg = join_rows_hgdp1kg_tobwgs.select_entries(
-        join_rows_hgdp1kg_tobwgs.GT
-    )
+    select_entries_hgdp1kg = hgdp1kg_filt.select_entries(hgdp1kg_filt.GT)
     select_entries_tobwgs = change_lgt_to_gt.select_entries(change_lgt_to_gt.GT)
     # Only keep columns we need
     select_columns_hgdp1kg = select_entries_hgdp1kg.select_cols(
@@ -51,38 +61,15 @@ def query(output):  # pylint: disable=too-many-locals
     # Join datasets
     hgdp1kg_tobwgs_joined = hgdp1kg_filtered.union_cols(tobwgs_filtered)
 
-    # liftover and get variants
-    ht_gnomad_loadings = hl.read_table(GNOMAD_V2_LOADINGS)
-    ht_gnomad_loadings.count()
-    # 94176
-    rg37 = hl.get_reference('GRCh37')
-    rg38 = hl.get_reference('GRCh38')
-    rg37.add_liftover(
-        'gs://hail-common/references/grch37_to_grch38.over.chain.gz', rg38
-    )
-    ht_gnomad_loadings_liftover = ht_gnomad_loadings.annotate(
-        liftover=hl.liftover(ht_gnomad_loadings.locus, 'GRCh38', include_strand=False),
-        old_locus=ht_gnomad_loadings.locus,
-    )
-    ht_gnomad_loadings_liftover = ht_gnomad_loadings_liftover.key_by(
-        locus=ht_gnomad_loadings_liftover.liftover
-    )
-
-    # add liftover into the filtered, combined matrix table
-    hgdp1kg_tobwgs_liftover = hgdp1kg_tobwgs_joined.annotate_rows(
-        rows_to_keep=ht_gnomad_loadings_liftover[hgdp1kg_tobwgs_joined.locus]
-    )
-    hgdp1kg_tobwgs_filt = hgdp1kg_tobwgs_liftover.filter_rows(
-        hgdp1kg_tobwgs_liftover.locus == hgdp1kg_tobwgs_liftover.rows_to_keep.liftover
-    )
-
     # Perform PCA
     eigenvalues_path = f'{output}/eigenvalues.csv'
     scores_path = f'{output}/scores.ht'
     loadings_path = f'{output}/loadings.ht'
 
     # test on 100 samples
-    mt_head = hgdp1kg_tobwgs_filt.head(n=hgdp1kg_tobwgs_filt.count_rows(), n_cols=100)
+    mt_head = hgdp1kg_tobwgs_joined.head(
+        n=hgdp1kg_tobwgs_joined.count_rows(), n_cols=100
+    )
     eigenvalues, scores, loadings = hl.hwe_normalized_pca(
         mt_head.GT, compute_loadings=True, k=20
     )
