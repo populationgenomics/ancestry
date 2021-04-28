991f5fe5ffc72d2b5b34ed7c76a8b96f77f8ece2
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-04-28 16:46:31 +1000

    Merge HGDP + 1KG dataset with TOB-WGS and PCA (#13)
    * Merge HGDP + 1KG dataset with TOB-WGS and PCA
    
    This script combines the HGDP + 1KG dataset with the TOB-WGS dataset, then filters rows to the 90K variants from gnomAD. Once filtered, a PCA is performed.
    
    * Change filtering order and moving liftover step to a new script
    
    I reordered the filtering loci step so that it happens before joining datasets, as joins are computationally expensive so this should lower the processing time. I also created a new script (available in gnomad_90k_variants_liftover), which performs a liftover of the gnomad v2.1 loadings (from hg37 to hg38). As suggested by Vlad, I also rekeyed mt_tob_wgs, so that it has both the locus and alleles as its keys (and that way, has the same keys as mt_hgdp_1kg).
    
    * Change tob-wgs input path to new main path
    
    Vlad moved the tob-wgs dataset from `gs://cpg-tob-wgs-temporary/joint_vcf/v1/work/combiner/genomes.mt` to `gs://cpg-tob-wgs-main/joint_vcf/v1/raw/genomes.mt`. I've updated the path so it now reflects the new location on main.
    
    * Update scripts/hail_batch/hgdp1kg_tobwgs_pca/hgdp_1kg_tob_wgs_pca.py
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    * Filter tables 1 and 2 using a semi_join with loadings
    
    I filtered tables 1 and 2 using a semi_join with loadings, then joined datasets by merging columns. As done previously, columns must be the same between datasets, so only 'GT' was selected, as this is used for the PCA step. In addition, I changed the outputdir option from gs://cpg-ancestry-temporary to gs://cpg-ancestry-analysis.
    
    * Update scripts/hail_batch/hgdp1kg_tobwgs_pca/hgdp_1kg_tob_wgs_pca.py
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    * Update scripts/hail_batch/hgdp1kg_tobwgs_pca/hgdp_1kg_tob_wgs_pca.py
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    * Test data on 50 samples from each dataset and incorporate name consistency
    
    I chose 50 samples from both the HGDP/1kG and TOB-WGS datasets before merging (this was a lot easier to do before than after), then merged the data. I also changed the variable naming to be more consistent (e.g., reassigning hgdp_1kg). Additionally, instead of selecting non-key columns, then dropping them, I used select_cols() with no named arguments. This automatically choses the keyed column as the column to select.
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

---
+++ scripts/hail_batch/hgdp1kg_tobwgs_pca/hgdp_1kg_tob_wgs_pca.py
@@ -0,0 +1,62 @@
+"""Generates loadings, scores, and eigenvalues for the HGDP,1KG, and tob-wgs dataset"""
+
+import click
+import pandas as pd
+import hail as hl
+from hail.experimental import lgt_to_gt
+
+GNOMAD_HGDP_1KG_MT = (
+    'gs://gcp-public-data--gnomad/release/3.1/mt/genomes/'
+    'gnomad.genomes.v3.1.hgdp_1kg_subset_dense.mt'
+)
+
+TOB_WGS = 'gs://cpg-tob-wgs-main/joint_vcf/v1/raw/genomes.mt'
+
+GNOMAD_LIFTOVER_LOADINGS = 'gs://cpg-reference/gnomad/gnomad_loadings_90k_liftover.ht'
+
+
+@click.command()
+@click.option('--output', help='GCS output path', required=True)
+def query(output):  # pylint: disable=too-many-locals
+    """Query script entry point."""
+
+    hl.init(default_reference='GRCh38')
+
+    hgdp_1kg = hl.read_matrix_table(GNOMAD_HGDP_1KG_MT)
+    tob_wgs = hl.read_matrix_table(TOB_WGS).key_rows_by('locus', 'alleles')
+    loadings = hl.read_table(GNOMAD_LIFTOVER_LOADINGS).key_by('locus', 'alleles')
+
+    # filter to loci that are contained in both tables and the loadings
+    hgdp_1kg = hgdp_1kg.filter_rows(
+        hl.is_defined(loadings.index(hgdp_1kg['locus'], hgdp_1kg['alleles']))
+        & hl.is_defined(tob_wgs.index_rows(hgdp_1kg['locus'], hgdp_1kg['alleles']))
+    )
+    tob_wgs = tob_wgs.semi_join_rows(hgdp_1kg.rows())
+
+    # Entries and columns must be identical
+    tob_wgs = tob_wgs.select_entries(GT=lgt_to_gt(tob_wgs.LGT, tob_wgs.LA))
+    hgdp_1kg = hgdp_1kg.select_entries(hgdp_1kg.GT)
+    hgdp_1kg = hgdp_1kg.select_cols()
+    # select the first 50 samples from each dataset as a subset to test
+    tob_wgs = tob_wgs.head(None, n_cols=50)
+    hgdp_1kg = hgdp_1kg.head(None, n_cols=50)
+    # Join datasets
+    hgdp1kg_tobwgs_joined = hgdp_1kg.union_cols(tob_wgs)
+
+    # Perform PCA
+    eigenvalues_path = f'{output}/eigenvalues.csv'
+    scores_path = f'{output}/scores.ht'
+    loadings_path = f'{output}/loadings.ht'
+    eigenvalues, scores, loadings = hl.hwe_normalized_pca(
+        hgdp1kg_tobwgs_joined.GT, compute_loadings=True, k=20
+    )
+    # save the list of eigenvalues
+    eigenvalues_df = pd.DataFrame(eigenvalues)
+    eigenvalues_df.to_csv(eigenvalues_path, index=False)
+    # save the scores and loadings as a hail table
+    scores.write(scores_path, overwrite=True)
+    loadings.write(loadings_path, overwrite=True)
+
+
+if __name__ == '__main__':
+    query()  # pylint: disable=no-value-for-parameter
