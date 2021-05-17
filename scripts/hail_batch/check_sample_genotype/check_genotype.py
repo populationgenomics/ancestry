8ce6572c4b23de7f97cf183f209a12e0d877027c
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-05-17 11:01:23 +1000

    Save data as 'tsv' rather than 'feather'
    After unsuccessfully trying to save the output files, I'm saving output at the most basic level and outputing them as .tsv files. I also changed the number of workers from primary to secondary.

--- scripts/hail_batch/check_sample_genotype/check_genotype.py
+++ scripts/hail_batch/check_sample_genotype/check_genotype.py
@@ -1,8 +1,6 @@
 """Get genotypes of samples"""
 
 import click
-import pandas as pd
-from pyarrow import feather
 import hail as hl
 
 HGDP1KG_TOBWGS = (
@@ -26,22 +24,13 @@ def query(output):  # pylint: disable=too-many-locals
     hgdp_1kg = hl.read_matrix_table(GNOMAD_HGDP_1KG_MT)
     amr = hgdp_1kg.filter_cols(hgdp_1kg.population_inference.pop == 'amr')
     amr = amr.annotate_rows(gt_stats=hl.agg.call_stats(amr.GT, amr.alleles))
-    amr_to_pandas = amr.rows().to_pandas()
-    amr_af = pd.DataFrame(amr_to_pandas['gt_stats.AF'])
-    amr_af_path = f'{output}/amr_AF.feather'
-    feather.write_feather(
-        amr_af,
-        amr_af_path,
-    )
-    # get three TOB-WGS samples
+    amr_af_path = f'{output}/amr.tsv'
+    amr.gt_stats.AF.export(amr_af_path)
+
+    # get TOB-WGS LGT data for downstream manipulation
     mt = hl.read_matrix_table(HGDP1KG_TOBWGS)
-    mt_filt = mt.filter_cols(
-        (mt.s == 'TOB1524') | (mt.s == 'TOB1532') | (mt.s == 'TOB1533')
-    )
-    mt_filt = hl.sample_qc(mt_filt)
-    mt_filt_to_pandas = mt_filt.cols().to_pandas()
-    mt_filt_path = f'{output}/tob_wgs_selected_sample_qc.csv'
-    mt_filt_to_pandas.to_csv(mt_filt_path, index=False)
+    mt_filt_path = f'{output}/tob_wgs_LGT.tsv'
+    mt.LGT.export(mt_filt_path)
 
 
 if __name__ == '__main__':
