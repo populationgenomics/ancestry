c860618e3c2fec4917244e7c861f3a313d70130c
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-05-25 12:01:23 +1000

    Add script to copy tob-wgs PCA files to the test bucket

---
+++ scripts/copy_1kg_hgdp_tob_wgs_pca.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ex
+
+# Copy a subset of the data to the `test` bucket.
+gsutil cp gs://cpg-tob-wgs-analysis/1kg_hgdp_tobwgs_pca/v1/hgdp1kg_tobwgs_joined_all_samples.mt gs://cpg-tob-wgs-test/1kg_hgdp_tobwgs_pca/
+gsutil cp gs://cpg-tob-wgs-analysis/1kg_hgdp_nfe/v0/loadings.ht gs://cpg-tob-wgs-test/1kg_hgdp_tobwgs_pca/
