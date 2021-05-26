4df1ac034fde78e91b2e83e42f1f0cf36bf1b0aa
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-05-26 19:29:49 +1000

    Add densify function before calculating AF for TOB-WGS data (#38)
    * Add densify function before calculating AF for TOB-WGS data
    
    I tested the densify function on a small subset of the TOB-WGS data, prior to calling the `lgt_to_gt` function. The AF changes after calling this function, and therefore I'm curious to see whether this has anything to do with the strange AF distribution we were seeing for the TOB-WGS data. This script is intended to get the AF after densifying the data, so that I can see how it affects the AF for the entire dataset.
    
    * Update scripts/hail_batch/hgdp1kg_tobwgs_densify/hgdp_1kg_tob_wgs_densify.py
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

---
+++ scripts/hail_batch/hgdp1kg_tobwgs_densify/main.py
@@ -0,0 +1,28 @@
+"""Entry point for the analysis runner."""
+
+import os
+import hail as hl
+import hailtop.batch as hb
+from analysis_runner import dataproc
+
+OUTPUT = os.getenv('OUTPUT')
+assert OUTPUT
+
+hl.init(default_reference='GRCh38')
+
+service_backend = hb.ServiceBackend(
+    billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
+)
+
+batch = hb.Batch(name=f'densify_tobwgs', backend=service_backend)
+
+dataproc.hail_dataproc_job(
+    batch,
+    f'hgdp_1kg_tob_wgs_densify.py --output={OUTPUT}',
+    max_age='24h',
+    num_workers=50,
+    packages=['click'],
+    job_name=f'densify_tobwgs',
+)
+
+batch.run()
