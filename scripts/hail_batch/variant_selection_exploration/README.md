0bf3e61364b60deb2468bdde709393250185df5b
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-05-31 17:53:58 +1000

    Explore possible AF cutoffs for variant selection script (#42)
    * Explore possible AF cutoffs for variant selection script
    
    Since my variant selection script uses more variants than we have previously used, and therefore will be compuationally expensive for the following LD-pruning step, this script is intended to explore how many variants we have at different AF thresholds.
    
    * Update scripts/hail_batch/variant_selection_exploration/README.md
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

---
+++ scripts/hail_batch/variant_selection_exploration/README.md
@@ -0,0 +1,9 @@
+# Variant selection exploration for HGDP/1kG + TOB-WGS data
+
+This runs a Hail query script in Dataproc using Hail Batch in order to select a set of variants from the HGDP/1kG + TOB-WGS data, based off of gnomAD v3 parameters. To run, use conda to install the analysis-runner, then execute the following command:
+
+```sh
+analysis-runner --dataset tob-wgs \
+--access-level standard --output-dir "gs://cpg-tob-wgs-web/tob_wgs_hgdp_1kg_variant_selection_exploration/v0" \
+--description "variant selection exploration" python3 main.py
+```
---
+++ scripts/hail_batch/variant_selection_exploration/main.py
@@ -0,0 +1,27 @@
+"""Run hgdp_1kg_tob_wgs_variant_selection.py using the analysis runner."""
+
+import os
+import hail as hl
+import hailtop.batch as hb
+from analysis_runner import dataproc
+
+OUTPUT = os.getenv('OUTPUT')
+assert OUTPUT
+
+hl.init(default_reference='GRCh38')
+
+service_backend = hb.ServiceBackend(
+    billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
+)
+
+batch = hb.Batch(name='variant selection exploration', backend=service_backend)
+
+dataproc.hail_dataproc_job(
+    batch,
+    f'hgdp_1kg_tob_wgs_variant_selection_exploration.py --output={OUTPUT}',
+    max_age='5h',
+    packages=['click', 'selenium'],
+    job_name='variant-selection-exploration',
+)
+
+batch.run()
