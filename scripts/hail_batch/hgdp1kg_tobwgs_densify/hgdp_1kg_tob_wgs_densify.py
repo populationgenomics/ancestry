8a85d0e5138184960b38c334590079a2b501389e
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-06-02 15:23:13 +1000

    Update scripts/hail_batch/hgdp1kg_tobwgs_densify/hgdp_1kg_tob_wgs_densify.py
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

--- scripts/hail_batch/hgdp1kg_tobwgs_densify/hgdp_1kg_tob_wgs_densify.py
+++ scripts/hail_batch/hgdp1kg_tobwgs_densify/hgdp_1kg_tob_wgs_densify.py
@@ -35,7 +35,7 @@ def query(output):  # pylint: disable=too-many-locals
         & hl.is_defined(tob_wgs.index_rows(hgdp_1kg['locus'], hgdp_1kg['alleles']))
     )
     tob_wgs = tob_wgs.semi_join_rows(hgdp_1kg.rows())
-    tob_wgs_path = f'{output}/tob_wgs_filtered_densified.mt'
+    tob_wgs_path = f'{output}/tob_wgs_filtered.mt'
     tob_wgs = tob_wgs.checkpoint(tob_wgs_path)
 
 
