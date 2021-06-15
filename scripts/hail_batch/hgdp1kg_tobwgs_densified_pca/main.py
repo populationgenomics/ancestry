ae363ffb2bae82fb2eb55759cf70dc4e6154c821
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-06-15 11:49:29 +1000

    Filter TOB-WGS data based off of output from hgdp_1kg_tob_wgs_densify.py
    Since we previously saved the densified, filtered TOB-WGS data (that is, variants which are present in the gnomAD loadings table), this script uses that saved table in order to filter the hgdp/1kg matrix table. The combined hgdp/1kg + TOB-WGS matrix table is used to perform PCA.

---
+++ scripts/hail_batch/hgdp1kg_tobwgs_densified_pca/main.py
@@ -0,0 +1,28 @@
+"""Entry point for the analysis runner."""
+
+import os
+import hail as hl
+import hailtop.batch as hb
+from analysis_runner import dataproc
+
+OUTPUT = os.getenv('OUTPUT')
+assert OUTPUT
+
+hl.init(default_reference='GRCh38')
+
+service_backend = hb.ServiceBackend(
+    billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
+)
+
+batch = hb.Batch(name=f'densify_tobwgs_pca', backend=service_backend)
+
+dataproc.hail_dataproc_job(
+    batch,
+    f'hgdp_1kg_tob_wgs_densified_pca.py --output={OUTPUT}',
+    max_age='12h',
+    num_secondary_workers=20,
+    packages=['click'],
+    job_name=f'densify_tobwgs_pca',
+)
+
+batch.run()
