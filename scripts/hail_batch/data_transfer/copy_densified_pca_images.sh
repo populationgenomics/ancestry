f56bdc972a4155ae4a4bab663f422721c6b260dd
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-06-18 10:30:54 +1000

    Update scripts/hail_batch/data_transfer/copy_densified_pca_images.sh
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

--- scripts/hail_batch/data_transfer/copy_densified_pca_images.sh
+++ scripts/hail_batch/data_transfer/copy_densified_pca_images.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-gsutil cp -r "gs://cpg-tob-wgs-main/tob_wgs_hgdp_1kg_nfe_pca_densified/v0" $OUTPUT
+gsutil cp -r "gs://cpg-tob-wgs-main/tob_wgs_hgdp_1kg_nfe_pca_densified/v0/*.png" $OUTPUT
