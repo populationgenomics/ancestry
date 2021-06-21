a5e5bd6bc448063fc6f9e2dbbf60716178569b2d
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-06-21 18:12:48 +1000

    Plot loadings of densified, NFE loadings (#66)
    This script utilises the manhattan plot function from hail to plot the loadings from NFE samples from the combined HGDP/1kG + TOB-WGS dataset, after densifying. I could not use the original function, as it's intended for GWAS studies and displays the y-axis as a -log10 p-value.

---
+++ scripts/plotting/hgdp_1kg_tob_wgs_pop_pca_densified_nfe_loadings/main.py
@@ -0,0 +1,28 @@
+"""Entry point for the analysis runner."""
+
+import os
+import hail as hl
+import hailtop.batch as hb
+from analysis_runner import dataproc
+
+OUTPUT = os.getenv('OUTPUT')
+assert OUTPUT
+
+hl.init(default_reference='GRCh38')
+
+service_backend = hb.ServiceBackend(
+    billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
+)
+
+batch = hb.Batch(name='densified loadings nfe', backend=service_backend)
+
+dataproc.hail_dataproc_job(
+    batch,
+    f'hgdp_1kg_tob_wgs_plot_loadings_nfe.py --output={OUTPUT}',
+    max_age='1h',
+    packages=['click', 'selenium'],
+    init=['gs://cpg-reference/hail_dataproc/install_phantomjs.sh'],
+    job_name=f'densified loadings nfe',
+)
+
+batch.run()
