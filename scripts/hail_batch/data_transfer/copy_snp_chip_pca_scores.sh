7a888d33ca67809f6df094f63f1c1bf9961dbe72
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-06-30 11:08:30 +1000

    Change reading and writing functions from mt syntax to table syntax

--- scripts/hail_batch/data_transfer/copy_snp_chip_pca_scores.sh
+++ scripts/hail_batch/data_transfer/copy_snp_chip_pca_scores.sh
@@ -12,6 +12,6 @@ output = os.getenv('OUTPUT')
 
 INPUT_MT = 'gs://cpg-tob-wgs-main/tob_wgs_snp_chip_variant_pca/v2/scores.ht'
 
-mt = read_matrix_table(INPUT_MT)
+mt = hl.read_table(INPUT_MT)
 output_path = f'{output}/scores.ht'
-mt.head(n_rows = 10000).write(output_path)
+mt.head(n = 10000).write(output_path)
