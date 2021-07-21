e8a568302c292e3e8a4b8b0a40fae1092f15b8a3
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-07-21 14:26:22 +1000

    Update TOB-WGS file from mt in `test` to mt in `main`

--- scripts/hail_batch/increase_snp_chip_partitions/increase_snp_chip_partitions.py
+++ scripts/hail_batch/increase_snp_chip_partitions/increase_snp_chip_partitions.py
@@ -7,8 +7,7 @@ import hail as hl
 from analysis_runner import bucket_path, output_path
 
 SNP_CHIP = bucket_path('snpchip/v1/snpchip_grch38.mt')
-# TOB_WGS = bucket_path('mt/v3-raw.mt')
-TOB_WGS = 'gs://cpg-tob-wgs-test/mt/v4.mt/'
+TOB_WGS = bucket_path('mt/v3-raw.mt')
 
 
 def query():
