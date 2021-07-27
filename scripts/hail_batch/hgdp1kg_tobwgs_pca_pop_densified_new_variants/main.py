45f037ba0a56a99643a82f411201587aaa74e35d
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-07-27 19:57:49 +1000

    Test pop PCA for new variant selection

---
+++ scripts/hail_batch/hgdp1kg_tobwgs_pca_pop_densified_new_variants/main.py
@@ -0,0 +1,26 @@
+"""Entry point for the analysis runner."""
+
+import os
+import sys
+import hailtop.batch as hb
+from analysis_runner import dataproc
+
+
+POP = sys.argv[1] if len(sys.argv) > 1 else 'nfe'
+
+service_backend = hb.ServiceBackend(
+    billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
+)
+
+batch = hb.Batch(name=f'{POP} pca', backend=service_backend)
+
+dataproc.hail_dataproc_job(
+    batch,
+    f'hgdp_1kg_tob_wgs_pop_pca_densified.py --pop {POP}',
+    max_age='4h',
+    num_secondary_workers=20,
+    packages=['click'],
+    job_name=f'{POP}-pca-new-variants',
+)
+
+batch.run()
