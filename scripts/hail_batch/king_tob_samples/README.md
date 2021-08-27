2c5d2b45b001ce2d58db3fe6c7a019cf05b54b5b
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-08-27 18:39:47 +1000

    Identify related individuals and save output to html

--- scripts/hail_batch/king_tob_samples/README.md
+++ scripts/hail_batch/king_tob_samples/README.md
@@ -1,9 +1,9 @@
-# Estimate relatedness between NFE samples using King
+# Estimate relatedness between TOB samples using King
 
-This runs a Hail query script in Dataproc using Hail Batch in order to estimate relatedness between NFE samples from the HGDP/1KG datasets using the KING relatedness estimator. To run, use conda to install the analysis-runner, then execute the following command:
+This runs a Hail query script in Dataproc using Hail Batch in order to estimate relatedness between TOB samples from the HGDP/1KG datasets using the KING relatedness estimator. To run, use conda to install the analysis-runner, then execute the following command:
 
 ```sh
 analysis-runner --dataset tob-wgs \
---access-level test --output-dir "king/v0" \
---description "king nfe samples" python3 main.py
+--access-level standard --output-dir "king/v0" \
+--description "king tob samples" python3 main.py
 ```
--- scripts/hail_batch/king_tob_samples/king_tob.py
+++ scripts/hail_batch/king_tob_samples/king_tob.py
@@ -3,6 +3,7 @@ Estimate kinship coefficient using KING on TOB samples from the HGDP/1KG dataset
 """
 
 import hail as hl
+import pandas as pd
 from analysis_runner import bucket_path, output_path
 
 HGDP1KG_TOBWGS = bucket_path(
@@ -21,6 +22,22 @@ def query():
     king = hl.king(mt.GT)
     king_path = output_path('king_kinship_estimate_TOB.ht')
     king.write(king_path)
+    ht = king.entries()
+    related_samples = ht.filter((ht.s_1 != ht.s) & (ht.phi > 0.125), keep=True)
+    struct = hl.struct(i=related_samples.s_1, j=related_samples.s)
+    struct = struct.annotate(phi=related_samples.phi)
+    related_samples_to_remove = hl.maximal_independent_set(
+        struct.i, struct.j, False  # pylint: disable=E1101
+    )
+    n_related_samples = related_samples_to_remove.count()
+    print(f'related_samples_to_remove.count() = {n_related_samples}')
+    # save as html
+    html = pd.DataFrame(
+        {'related_individual': related_samples_to_remove.node.s.collect()}
+    ).to_html()
+    plot_filename_html = output_path(f'related_samples.html', 'web')
+    with hl.hadoop_open(plot_filename_html, 'w') as f:
+        f.write(html)
 
 
 if __name__ == '__main__':
