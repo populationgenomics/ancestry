b05d8fde49bd68536e9669e95b922779735dad7e
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-08-31 18:06:52 +1000

    Plot relatedness estimates for both NFE and global samples

--- scripts/plotting/hgdp1kg_tobwgs_pc_relate_unpruned/hgdp_1kg_tob_wgs_related_samples.py
+++ scripts/plotting/hgdp1kg_tobwgs_pc_relate_unpruned/hgdp_1kg_tob_wgs_related_samples.py
@@ -8,12 +8,12 @@ import pandas as pd
 from analysis_runner import bucket_path, output_path
 
 
-# KINSHIP_ESTIMATE = bucket_path(
-#     'tob_wgs_hgdp_1kg_pc_relate/v0/pc_relate_kinship_estimate.ht/'
-# )
-
-KINSHIP_ESTIMATE = bucket_path(
+KINSHIP_ESTIMATE_NFE = bucket_path(
     'tob_wgs_hgdp_1kg_nfe_pc_relate/v1/pc_relate_kinship_estimate.ht'
+)
+
+KINSHIP_ESTIMATE_GLOBAL = bucket_path(
+    'tob_wgs_hgdp_1kg_pc_relate/v0/pc_relate_kinship_estimate.ht'
 )
 
 
@@ -22,7 +22,8 @@ def query():
 
     hl.init(default_reference='GRCh38')
 
-    ht = hl.read_table(KINSHIP_ESTIMATE)
+    # plot relatedness estimates for global populations
+    ht = hl.read_table(KINSHIP_ESTIMATE_GLOBAL)
 
     related_samples = ht.filter(ht.kin > 0.1)
 
@@ -34,7 +35,24 @@ def query():
             'kin': related_samples.kin.collect(),
         }
     ).to_html()
-    plot_filename_html = output_path(f'pc_relate_unpruned_matrix.html', 'web')
+    plot_filename_html = output_path(f'pc_relate_global_matrix.html', 'web')
+    with hl.hadoop_open(plot_filename_html, 'w') as f:
+        f.write(html)
+
+    # plot relatedness estimates for NFE samples
+    ht = hl.read_table(KINSHIP_ESTIMATE_NFE)
+
+    related_samples = ht.filter(ht.kin > 0.1)
+
+    # save as html
+    html = pd.DataFrame(
+        {
+            'i_s': related_samples.i.s.collect(),
+            'j_s': related_samples.j.s.collect(),
+            'kin': related_samples.kin.collect(),
+        }
+    ).to_html()
+    plot_filename_html = output_path(f'pc_relate_nfe_matrix.html', 'web')
     with hl.hadoop_open(plot_filename_html, 'w') as f:
         f.write(html)
 
