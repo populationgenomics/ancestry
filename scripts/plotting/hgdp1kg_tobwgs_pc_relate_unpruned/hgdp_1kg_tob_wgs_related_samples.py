f699cc49be576f52e9949ec544dd91e1f8887ef4
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-08-31 18:14:19 +1000

    Plot kinship scores of related individuals after running pc_relate (#145)
    * Plot kinship scores of related individuals after running pc_relate
    
    * Plot relatedness estimates for both NFE and global samples

---
+++ scripts/plotting/hgdp1kg_tobwgs_pc_relate_unpruned/hgdp_1kg_tob_wgs_related_samples.py
@@ -0,0 +1,61 @@
+"""
+Plot scores of related individuals after running pc_relate.
+
+"""
+
+import hail as hl
+import pandas as pd
+from analysis_runner import bucket_path, output_path
+
+
+KINSHIP_ESTIMATE_NFE = bucket_path(
+    'tob_wgs_hgdp_1kg_nfe_pc_relate/v1/pc_relate_kinship_estimate.ht'
+)
+
+KINSHIP_ESTIMATE_GLOBAL = bucket_path(
+    'tob_wgs_hgdp_1kg_pc_relate/v0/pc_relate_kinship_estimate.ht'
+)
+
+
+def query():
+    """Query script entry point."""
+
+    hl.init(default_reference='GRCh38')
+
+    # plot relatedness estimates for global populations
+    ht = hl.read_table(KINSHIP_ESTIMATE_GLOBAL)
+
+    related_samples = ht.filter(ht.kin > 0.1)
+
+    # save as html
+    html = pd.DataFrame(
+        {
+            'i_s': related_samples.i.s.collect(),
+            'j_s': related_samples.j.s.collect(),
+            'kin': related_samples.kin.collect(),
+        }
+    ).to_html()
+    plot_filename_html = output_path(f'pc_relate_global_matrix.html', 'web')
+    with hl.hadoop_open(plot_filename_html, 'w') as f:
+        f.write(html)
+
+    # plot relatedness estimates for NFE samples
+    ht = hl.read_table(KINSHIP_ESTIMATE_NFE)
+
+    related_samples = ht.filter(ht.kin > 0.1)
+
+    # save as html
+    html = pd.DataFrame(
+        {
+            'i_s': related_samples.i.s.collect(),
+            'j_s': related_samples.j.s.collect(),
+            'kin': related_samples.kin.collect(),
+        }
+    ).to_html()
+    plot_filename_html = output_path(f'pc_relate_nfe_matrix.html', 'web')
+    with hl.hadoop_open(plot_filename_html, 'w') as f:
+        f.write(html)
+
+
+if __name__ == '__main__':
+    query()
