ebb0e50d1431639ab50f402d786fd4011ecdd51a
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-09-01 17:33:41 +1000

    Add one more html file for KING relatedness estimates (#147)

--- scripts/plotting/hgdp1kg_tobwgs_pc_relate_unpruned/hgdp_1kg_tob_wgs_related_samples.py
+++ scripts/plotting/hgdp1kg_tobwgs_pc_relate_unpruned/hgdp_1kg_tob_wgs_related_samples.py
@@ -16,13 +16,17 @@ KINSHIP_ESTIMATE_GLOBAL = bucket_path(
     'tob_wgs_hgdp_1kg_pc_relate/v0/pc_relate_kinship_estimate.ht'
 )
 
+KING_ESTIMATE_NFE = bucket_path(
+    'gs://cpg-tob-wgs-main/king/v0/king_kinship_estimate_NFE.ht'
+)
+
 
 def query():
     """Query script entry point."""
 
     hl.init(default_reference='GRCh38')
 
-    # plot relatedness estimates for global populations
+    # plot relatedness estimates for pc_relate global populations
     ht = hl.read_table(KINSHIP_ESTIMATE_GLOBAL)
 
     related_samples = ht.filter(ht.kin > 0.1)
@@ -39,7 +43,7 @@ def query():
     with hl.hadoop_open(plot_filename_html, 'w') as f:
         f.write(html)
 
-    # plot relatedness estimates for NFE samples
+    # plot relatedness estimates for pc_relate NFE samples
     ht = hl.read_table(KINSHIP_ESTIMATE_NFE)
 
     related_samples = ht.filter(ht.kin > 0.1)
@@ -53,6 +57,24 @@ def query():
         }
     ).to_html()
     plot_filename_html = output_path(f'pc_relate_nfe_matrix.html', 'web')
+    with hl.hadoop_open(plot_filename_html, 'w') as f:
+        f.write(html)
+
+    # plot relatedness estimates for KING NFE samples
+    mt = hl.read_matrix_table(KINSHIP_ESTIMATE_NFE)
+    ht = mt.entries()
+    related_samples = ht.filter(ht.s_1 != ht.s)
+    related_samples = ht.filter(ht.phi > 0.1)
+
+    # save as html
+    html = pd.DataFrame(
+        {
+            'i_s': related_samples.s_1.collect(),
+            'j_s': related_samples.s.collect(),
+            'kin': related_samples.phi.collect(),
+        }
+    ).to_html()
+    plot_filename_html = output_path(f'king_nfe_90k_pruned.html', 'web')
     with hl.hadoop_open(plot_filename_html, 'w') as f:
         f.write(html)
 
