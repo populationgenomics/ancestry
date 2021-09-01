77eec8cc5185a5cea0449cfe3263d2f23fe08ef1
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-09-01 15:02:39 +1000

    Test pc_relate on entire global dataset

--- scripts/hail_batch/pc_relate_nfe_samples/pc_relate_nfe.py
+++ scripts/hail_batch/pc_relate_nfe_samples/pc_relate_nfe.py
@@ -5,9 +5,8 @@ Perform pc_relate on nfe samples from the HGDP/1KG dataset.
 import hail as hl
 from analysis_runner import output_path
 
-GNOMAD_HGDP_1KG_MT = (
-    'gs://gcp-public-data--gnomad/release/3.1/mt/genomes/'
-    'gnomad.genomes.v3.1.hgdp_1kg_subset_dense.mt'
+HGDP_1KG_MT_SUBSAMPLED = (
+    'gs://cpg-tob-wgs-test-tmp/pc_relate/v2/hgdp_1kg_subsampled_10k.mt/'
 )
 
 
@@ -16,19 +15,14 @@ def query():
 
     hl.init(default_reference='GRCh38')
 
-    mt = hl.read_matrix_table(GNOMAD_HGDP_1KG_MT)
-    nrows_mt = mt.count_rows()
-    mt = mt.sample_rows(10000 / nrows_mt, seed=12345)
-    mt_path = output_path('hgdp_1kg_subsampled_10k.mt', 'tmp')
-    mt = mt.checkpoint(mt_path)
+    mt = hl.read_matrix_table(HGDP_1KG_MT_SUBSAMPLED)
     mt = mt.repartition(1000, shuffle=False)
-    mt = mt.filter_cols(mt.population_inference.pop == 'nfe')
-    mt_repartitioned_path = output_path('hgdp_1kg_repartitioned_10k.mt', 'tmp')
+    mt_repartitioned_path = output_path('hgdp_1kg_repartitioned_10k_global.mt', 'tmp')
     mt = mt.checkpoint(mt_repartitioned_path)
 
     # Remove related samples (at the 2nd degree or closer)
     king = hl.king(mt.GT)
-    king_path = output_path('king_kinship_estimate.ht')
+    king_path = output_path('king_kinship_estimate_global.ht')
     king.write(king_path)
 
 
