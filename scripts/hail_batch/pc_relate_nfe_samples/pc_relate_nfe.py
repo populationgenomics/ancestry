f67fbf66e5213214b92c759724402d2bda982823
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-09-07 13:21:06 +1000

    Retain related individuals with kinship > 0.1

--- scripts/hail_batch/pc_relate_nfe_samples/pc_relate_nfe.py
+++ scripts/hail_batch/pc_relate_nfe_samples/pc_relate_nfe.py
@@ -3,11 +3,10 @@ Perform pc_relate on nfe samples from the HGDP/1KG dataset.
 """
 
 import hail as hl
-from analysis_runner import output_path
+import pandas as pd
+from analysis_runner import bucket_path, output_path
 
-HGDP_1KG_MT_SUBSAMPLED = (
-    'gs://cpg-tob-wgs-test-tmp/pc_relate/v2/hgdp_1kg_subsampled_10k.mt/'
-)
+KING = bucket_path('pc_relate/v3/king_kinship_estimate_global.ht')
 
 
 def query():
@@ -15,15 +14,19 @@ def query():
 
     hl.init(default_reference='GRCh38')
 
-    mt = hl.read_matrix_table(HGDP_1KG_MT_SUBSAMPLED)
-    mt = mt.repartition(1000, shuffle=False)
-    mt_repartitioned_path = output_path('hgdp_1kg_repartitioned_10k_global.mt', 'tmp')
-    mt = mt.checkpoint(mt_repartitioned_path)
-
     # Remove related samples (at the 2nd degree or closer)
-    king = hl.king(mt.GT)
-    king_path = output_path('king_kinship_estimate_global.ht')
-    king.write(king_path)
+    king = hl.read_matrix_table(KING)
+    ht = king.entries()
+    related_samples = ht.filter((ht.s_1 != ht.s) & (ht.phi > 0.1), keep=True)
+    king_global = pd.DataFrame(
+        {
+            'i_s': related_samples.s_1.collect(),
+            'j_s': related_samples.s.collect(),
+            'kin': related_samples.phi.collect(),
+        }
+    )
+    filename = output_path(f'king_global_matrix_90k.csv', 'metadata')
+    king_global.to_csv(filename, index=False)
 
 
 if __name__ == '__main__':
