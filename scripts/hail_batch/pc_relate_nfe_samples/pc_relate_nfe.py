77a629f1ed689d445fabfda08a757c55f5670757
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-09-09 11:40:44 +1000

    Get maximal independent set and save file

--- scripts/hail_batch/pc_relate_nfe_samples/pc_relate_nfe.py
+++ scripts/hail_batch/pc_relate_nfe_samples/pc_relate_nfe.py
@@ -6,7 +6,8 @@ import hail as hl
 import pandas as pd
 from analysis_runner import bucket_path, output_path
 
-KING = bucket_path('pc_relate/v3/king_kinship_estimate_global.ht')
+KING_GLOBAL = bucket_path('pc_relate/v3/king_kinship_estimate_global.ht')
+KING_NFE = bucket_path('pc_relate/v2/king_kinship_estimate.ht')
 
 
 def query():
@@ -15,7 +16,7 @@ def query():
     hl.init(default_reference='GRCh38')
 
     # Remove related samples (at the 2nd degree or closer)
-    king = hl.read_matrix_table(KING)
+    king = hl.read_matrix_table(KING_GLOBAL)
     ht = king.entries()
     related_samples = ht.filter((ht.s_1 != ht.s) & (ht.phi > 0.1), keep=True)
     king_global = pd.DataFrame(
@@ -25,8 +26,58 @@ def query():
             'kin': related_samples.phi.collect(),
         }
     )
-    filename = output_path(f'king_global_matrix_10k.csv', 'metadata')
+    filename = output_path('king_global_matrix_10k.csv')
     king_global.to_csv(filename, index=False)
+
+    # get maximal independent set
+    second_degree_related_samples = ht.filter(
+        (ht.s_1 != ht.s) & (ht.phi > 0.125), keep=True
+    )
+    struct = hl.struct(
+        i=second_degree_related_samples.s_1, j=second_degree_related_samples.s
+    )
+    struct = struct.annotate(phi=second_degree_related_samples.phi)
+    related_samples_to_remove = hl.maximal_independent_set(
+        struct.i, struct.j, False  # pylint: disable=E1101
+    )
+    related_samples = pd.DataFrame(
+        {'related_individual': related_samples_to_remove.node.collect()}
+    )
+    filename = output_path(
+        'king_10k_global_related_samples_maximal_independent_set.csv'
+    )
+    related_samples.to_csv(filename, index=False)
+
+    # Remove related samples (at the 2nd degree or closer) for NFE samples
+    king_nfe = hl.read_matrix_table(KING_NFE)
+    ht = king_nfe.entries()
+    related_samples = ht.filter((ht.s_1 != ht.s) & (ht.phi > 0.1), keep=True)
+    king_nfe = pd.DataFrame(
+        {
+            'i_s': related_samples.s_1.collect(),
+            'j_s': related_samples.s.collect(),
+            'kin': related_samples.phi.collect(),
+        }
+    )
+    filename = output_path('king_nfe_matrix_10k.csv')
+    king_nfe.to_csv(filename, index=False)
+
+    # get maximal independent set
+    second_degree_related_samples = ht.filter(
+        (ht.s_1 != ht.s) & (ht.phi > 0.125), keep=True
+    )
+    struct = hl.struct(
+        i=second_degree_related_samples.s_1, j=second_degree_related_samples.s
+    )
+    struct = struct.annotate(phi=second_degree_related_samples.phi)
+    related_samples_to_remove = hl.maximal_independent_set(
+        struct.i, struct.j, False  # pylint: disable=E1101
+    )
+    related_samples = pd.DataFrame(
+        {'related_individual': related_samples_to_remove.node.collect()}
+    )
+    filename = output_path('king_10k_nfe_related_samples_maximal_independent_set.csv')
+    related_samples.to_csv(filename, index=False)
 
 
 if __name__ == '__main__':
