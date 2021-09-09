f989d3f47b4994f6a760b205a14cee19346c919f
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-09-09 19:55:41 +1000

    Add subset_hgdp.py

--- scripts/plotting/hgdp_1kg_tob_wgs_pca_new_variants/main.py
+++ scripts/plotting/hgdp_1kg_tob_wgs_pca_new_variants/main.py
@@ -8,15 +8,15 @@ service_backend = hb.ServiceBackend(
     billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
 )
 
-batch = hb.Batch(name='new-variants-plot-pca', backend=service_backend)
+batch = hb.Batch(name='subset-gnomad-hgdp-1kg', backend=service_backend)
 
 dataproc.hail_dataproc_job(
     batch,
-    'plot_pca_and_loadings.py',
-    max_age='2h',
+    'subset_hgdp.py',
+    max_age='12h',
     packages=['selenium'],
     init=['gs://cpg-reference/hail_dataproc/install_common.sh'],
-    job_name='new-variants-plot-pca',
+    job_name='subset-gnomad-hgdp-1kg',
 )
 
 batch.run()
