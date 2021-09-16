3df629c82f78f975fe8dd39f98c227e4ff576554
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-09-16 16:26:32 +1000

    Repartition lifted over gnomAD v2 loadings

---
+++ scripts/hail_batch/repartition_loadings/repartition_loadings.py
@@ -0,0 +1,21 @@
+"""Repartition lifted over gnomAD v2 loadings"""
+
+import hail as hl
+from analysis_runner import output_path
+
+LOADINGS = 'gs://cpg-reference/gnomad/gnomad_loadings_90k_liftover.ht'
+
+
+def query():
+    """Query script entry point."""
+
+    hl.init(default_reference='GRCh38')
+
+    loadings = hl.read_table(LOADINGS)
+    loadings = loadings.repartition(100, shuffle=False)
+    loadings_path = output_path('gnomad_loadings_90k_liftover_repartitioned.ht')
+    loadings.write(loadings_path)
+
+
+if __name__ == '__main__':
+    query()
