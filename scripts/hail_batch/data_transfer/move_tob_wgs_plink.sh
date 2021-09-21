ffe58fcc65ddf69300677c436b203e49e079f3bb
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-09-21 12:25:26 +1000

    Remove asterisk after folder name

--- scripts/hail_batch/data_transfer/move_tob_wgs_plink.sh
+++ scripts/hail_batch/data_transfer/move_tob_wgs_plink.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-gsutil -m -cp -r 'gs://cpg-tob-wgs-main/tob_wgs_plink/v0/*' "gs://cpg-tob-wgs-shared-jpowell/mt/plink/"
+gsutil -m -cp -r "gs://cpg-tob-wgs-main/tob_wgs_plink/v0/" "gs://cpg-tob-wgs-shared-jpowell/mt/plink/"
