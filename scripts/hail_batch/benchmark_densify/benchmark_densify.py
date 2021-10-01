0fd33ea103cad0ae792eeae64a100a86433d12ec
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-10-01 16:14:09 +1000

    Test file size when removing entries field

--- scripts/hail_batch/benchmark_densify/benchmark_densify.py
+++ scripts/hail_batch/benchmark_densify/benchmark_densify.py
@@ -1,9 +1,10 @@
 """Test cost and speed of densifying on 20 samples"""
 
 import hail as hl
-from analysis_runner import bucket_path, output_path
+from analysis_runner import output_path
 
-TOB_WGS = bucket_path('mt/v5.1.mt')
+SPARSE_MT = 'gs://cpg-tob-wgs-test-tmp/densify_benchmark/v5/sparse_tob_wgs.mt'
+DENSIFIED_MT = 'gs://cpg-tob-wgs-test-tmp/densify_benchmark/v5/densified_tob_wgs.mt'
 
 
 def query():
@@ -11,21 +12,22 @@ def query():
 
     hl.init(default_reference='GRCh38')
 
-    mt = hl.read_matrix_table(TOB_WGS)
-    # remove unecessary information from the mt
-    densified_mt = hl.experimental.densify(mt)
-    mt = mt.select_entries(mt.GT).select_globals().select_rows().select_cols()
-    densified_mt = (
-        densified_mt.select_entries(densified_mt.GT)
-        .select_globals()
-        .select_rows()
-        .select_cols()
-    )
-    # save output
-    mt_path = output_path(f'sparse_tob_wgs.mt', 'tmp')
-    densified_path = output_path(f'densified_tob_wgs.mt', 'tmp')
-    mt.write(mt_path)
-    densified_mt.write(densified_path)
+    sparse = hl.read_matrix_table(SPARSE_MT)
+    dense = hl.read_matrix_table(DENSIFIED_MT)
+    # save entries field only
+    dense_entries = dense.entries()
+    dense_entries_path = output_path(f'dense_entries.ht', 'tmp')
+    dense_entries.write(dense_entries_path)
+    sparse_entries = sparse.entries()
+    sparse_entries_path = output_path(f'sparse_entries.ht', 'tmp')
+    sparse_entries.write(sparse_entries_path)
+    # remove entries field and save
+    dense = dense.select_entries()
+    sparse = sparse.select_entries()
+    dense_path = output_path(f'dense_tob_wgs_no_entries.mt', 'tmp')
+    sparse_path = output_path(f'sparse_tob_wgs_no_entries.mt', 'tmp')
+    dense.write(dense_path)
+    sparse.write(sparse_path)
 
 
 if __name__ == '__main__':
