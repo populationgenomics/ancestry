8f3679d48840b4e271d6cf28e69072ebe58c9778
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-10-01 15:13:01 +1000

    Test file size after making entries, rows, columns, and globals equal

--- scripts/hail_batch/benchmark_densify/benchmark_densify.py
+++ scripts/hail_batch/benchmark_densify/benchmark_densify.py
@@ -1,9 +1,9 @@
 """Test cost and speed of densifying on 20 samples"""
 
 import hail as hl
-from analysis_runner import output_path
+from analysis_runner import bucket_path, output_path
 
-TOB_WGS = 'gs://cpg-tob-wgs-test-tmp/joint-calling/v6-25/combiner/v6-25-raw.mt'
+TOB_WGS = bucket_path('mt/v5.1.mt')
 
 
 def query():
@@ -12,10 +12,20 @@ def query():
     hl.init(default_reference='GRCh38')
 
     mt = hl.read_matrix_table(TOB_WGS)
-    mt = hl.experimental.densify(mt)
+    # remove unecessary information from the mt
+    densified_mt = hl.experimental.densify(mt)
+    mt = mt.select_entries(mt.GT).select_globals().select_rows().select_cols()
+    densified_mt = (
+        densified_mt.select_entries(densified_mt.GT)
+        .select_globals()
+        .select_rows()
+        .select_cols()
+    )
     # save output
-    mt_path = output_path(f'densified_tob_wgs.mt', 'tmp')
+    mt_path = output_path(f'sparse_tob_wgs.mt', 'tmp')
+    densified_path = output_path(f'densified_tob_wgs.mt', 'tmp')
     mt.write(mt_path)
+    densified_mt.write(densified_path)
 
 
 if __name__ == '__main__':
