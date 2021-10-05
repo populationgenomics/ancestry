1c2b1f3431f72e1da10dafa8f83f252474fb674f
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-10-05 12:58:23 +1100

    Change relational operator when filtering for MAF (#163)

--- scripts/hail_batch/af_tob_wgs/calculate_maf.py
+++ scripts/hail_batch/af_tob_wgs/calculate_maf.py
@@ -1,4 +1,4 @@
-"""Get number of variants with a MAF < 0.05"""
+"""Get number of variants with a MAF > 0.05"""
 
 import hail as hl
 from analysis_runner import bucket_path
@@ -14,12 +14,12 @@ def query():
     tob_wgs = hl.read_matrix_table(TOB_WGS)
     tob_wgs = hl.experimental.densify(tob_wgs)
     tob_wgs = hl.variant_qc(tob_wgs)
-    # get MAF < 0.05
+    # get MAF > 0.05
     tob_wgs = tob_wgs.filter_rows(tob_wgs.variant_qc.AF[0] < 1)
     snp_maf_05 = tob_wgs.aggregate_rows(
-        hl.agg.count_where(tob_wgs.variant_qc.AF[1] < 0.05)
+        hl.agg.count_where(tob_wgs.variant_qc.AF[1] > 0.05)
     )
-    print(f'Variant MAF < 0.05 = {snp_maf_05}')
+    print(f'Variant MAF > 0.05 = {snp_maf_05}')
 
 
 if __name__ == '__main__':
