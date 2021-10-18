423b43665e1626b99f0d769ac1fc3882d3132016
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-10-18 14:31:43 +1100

    Test hail batch on eQTL generation script

---
+++ scripts/hail_batch/eqtl_hail_batch/generate_eqtl_spearman.py
@@ -0,0 +1,117 @@
+"""Run Spearman rank correlation on SNPs and expression residuals"""
+
+import os
+import hailtop.batch as hb
+import pandas as pd
+import statsmodels.api as sm
+import statsmodels.stats.multitest as multi
+from patsy import dmatrices  # pylint: disable=no-name-in-module
+from scipy.stats import spearmanr
+
+INPUT_BUCKET = 'cpg-tob-wgs-test'
+OUTPUT_BUCKET = 'cpg-tob-wgs-test'
+DRIVER_IMAGE = os.getenv(
+    'DRIVER_IMAGE',
+    'australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:d2a9c316d6d752edb27623542c8a062db4466842-hail-0.2.73.devc6f6f09cec08',  # noqa: E501; pylint: disable=line-too-long
+)
+
+
+expression_df = pd.read_csv(
+    f'gs://{INPUT_BUCKET}/kat/input/Plasma_expression.tsv', sep='\t'
+)
+genotype_df = pd.read_csv(f'gs://{INPUT_BUCKET}/kat/input/genotype_chr22.tsv', sep='\t')
+geneLoc_df = pd.read_csv(f'gs://{INPUT_BUCKET}/kat/input/geneloc_chr22.tsv', sep='\t')
+snpLoc_df = pd.read_csv(f'gs://{INPUT_BUCKET}/kat/input/snpsloc_chr22.tsv', sep='\t')
+covariate_df = pd.read_csv(
+    f'gs://{INPUT_BUCKET}/kat/input/Plasma_peer_factors.tsv', sep='\t'
+)
+
+# Remove genes with 0 expression in all samples
+expression_df = expression_df.loc[:, (expression_df != 0).any(axis=0)]
+# Prepare variables used to calculate Spearman's correlation
+gene_ids = list(expression_df.columns.values)[1:]
+sample_ids = expression_df.iloc[:, 0]
+genotype_df = genotype_df[genotype_df.sampleid.isin(expression_df.sampleid)]
+snp_ids = list(genotype_df.columns.values)[1:]
+covariate_ids = list(covariate_df.columns.values)[1:]
+
+
+# Calculate expression residuals
+def calculate_residuals(gene_id):
+    """Calculate gene residuals"""
+    gene = gene_id
+    exprs_val = expression_df[['sampleid', gene]]
+    test_df = exprs_val.merge(covariate_df, on='sampleid', how='left')
+    test_df = test_df.rename(columns={test_df.columns[1]: 'expression'})
+    y, x = dmatrices(
+        'expression ~ sex + pc1 + pc2 + pc3 + pc4 + pc5 + pc6 + age + pf1 + pf2',
+        test_df,
+    )
+    model = sm.OLS(y, x)
+    residuals = list(model.fit().resid)
+    return residuals
+
+
+residual_df = pd.DataFrame(list(map(calculate_residuals, gene_ids))).T
+residual_df.columns = gene_ids
+residual_df = residual_df.assign(sampleid=list(sample_ids))
+
+# Get 1Mb sliding window around each gene
+geneLoc_df = geneLoc_df[geneLoc_df.geneid.isin(gene_ids)]
+geneLoc_df = geneLoc_df.assign(left=geneLoc_df.start - 1000000)
+geneLoc_df = geneLoc_df.assign(right=geneLoc_df.end + 1000000)
+
+
+# Run Spearman rank in parallel by sending genes in a batches
+def run_computation_in_scatter(idx):
+    """Run genes in scatter"""
+
+    def spearman_correlation(df):
+        """get Spearman rank correlation"""
+        gene = df.geneid
+        snp = df.snpid
+        res_val = residual_df[['sampleid', gene]]
+        genotype_val = genotype_df[['sampleid', snp]]
+        test_df = res_val.merge(genotype_val, on='sampleid', how='left')
+        test_df.columns = ['sampleid', 'residual', 'SNP']
+        coef, p = spearmanr(test_df['SNP'], test_df['residual'])
+        return (gene, snp, coef, p)
+
+    gene_info = geneLoc_df.iloc[idx]
+    snps_within_region = snpLoc_df[
+        snpLoc_df['pos'].between(gene_info['left'], gene_info['right'])
+    ]
+    gene_snp_df = snpLoc_df.merge(pd.DataFrame(snps_within_region))
+    gene_snp_df = gene_snp_df.assign(geneid=gene_info.geneid)
+    spearman_df = pd.DataFrame(list(gene_snp_df.apply(spearman_correlation, axis=1)))
+    spearman_df.columns = ['geneid', 'snpid', 'coef', 'p.value']
+    pvalues = spearman_df['p.value']
+    fdr_values = pd.DataFrame(list(multi.fdrcorrection(pvalues))).iloc[1]
+    spearman_df = spearman_df.assign(FDR=fdr_values)
+    return spearman_df
+
+
+backend = hb.ServiceBackend(billing_project='tob-wgs', bucket='cpg-tob-wgs-test')
+b = hb.Batch(name='eQTL', backend=backend, default_python_image=DRIVER_IMAGE)
+
+spearman_dfs_from_scatter = []
+for i in range(0, len(geneLoc_df.index)):
+    j = b.new_python_job(name=f'process_{i}')
+    result: hb.resource.PythonResult = j.call(run_computation_in_scatter, i)
+    spearman_dfs_from_scatter.append(result)
+
+
+def function_that_merges_dataframes(*df_list):
+    """Merge all Spearman dfs"""
+    print(df_list)
+    print(type(df_list))
+    merged_df = pd.concat(df_list)
+    return merged_df.to_string()
+
+
+merge_job = b.new_python_job(name='merge_scatters')
+result_second = merge_job.call(
+    function_that_merges_dataframes, *spearman_dfs_from_scatter
+)
+b.write_output(result_second.as_str(), f'gs://{OUTPUT_BUCKET}/kat/test.csv')
+b.run()
