faf6fd4bc3a1f8b2a2adb7e59cf584d4bfdf79e6
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-11-24 13:00:49 +1100

    Remove all sample outliers and generate PCA for NFE samples (#165)
    * Generate PCA for TOB samples
    
    * Add extra outliers after first PCA generation
    
    * Remove outliers after the second iteration
    
    * Add in new PCA outlliers falling 5 sds outside the mean
    
    * Test whether hgdp+1kg populations have clean clustering
    
    * Remove hgdp/1kg outliers
    
    * Remove outliers after latest iteration
    
    * Add outliers from first 5 PCs
    
    * Add outliers from the first 5 PCs
    
    * Remove additional outliers from first ten PCs
    
    * Update `mt_union_hgdp.mt` to latest version
    
    Update `mt_union_hgdp.mt` to latest version which doesn't have duplicated CPG samples
    
    * Remove outliers and redo process of outlier elimination
    
    I'm not sure why but Vlad had duplicated names in the original file, and that affected the results. I'm now rerunning this pipeline with the updated file that has duplicates removed
    
    * Remove outlier samples, round 1
    
    * Remove additional outliers
    
    * Add in additional outliers
    
    * Add in additional outliers
    
    * Update file path to newest v7 batch
    
    * Add new set of outliers
    
    * Add in HGDP/1kG outlier
    
    * Perform PCA at NFE level without removing outliers
    
    * Add in additional outliers
    
    * Add new outliers
    
    * Add in extra outliers
    
    * Add in extra outliers
    
    * Add in new outliers
    
    * Remove additional outliers
    
    * Add in extra outliers
    
    * Remove hashed out samples which are no longer outliers
    
    * Add in bucket path
    
    * Update README

---
+++ scripts/hail_batch/final_tob_batch_pca/generate_pca_no_outliers.py
@@ -0,0 +1,124 @@
+"""
+Perform pca on nfe samples from the HGDP/1KG +
+tob-wgs dataset and remove outlier samples.
+Reliant on output from
+```joint-calling/scripts/ancestry_pca.py```
+"""
+
+import hail as hl
+import pandas as pd
+from analysis_runner import bucket_path, output_path
+
+
+HGDP1KG_TOBWGS = bucket_path(f'joint-calling/v7/ancestry/mt_union_hgdp.mt', 'analysis')
+
+
+def query():
+    """Query script entry point."""
+    hl.init(default_reference='GRCh38')
+
+    mt = hl.read_matrix_table(HGDP1KG_TOBWGS)
+    # Get samples from the specified population only
+    mt = mt.filter_cols(
+        (mt.hgdp_1kg_metadata.population_inference.pop == 'nfe')
+        | (mt.s.contains('CPG'))
+    )
+    # remove outlier samples, as identified by PCA
+    outliers = [
+        'CPG2709',
+        'CPG2881',
+        'CPG3707',
+        'CPG1040',
+        'CPG2261',
+        'CPG9175',
+        'CPG9142',
+        'CPG4804',
+        'CPG2147',
+        'CPG3517',
+        'CPG3608',
+        'CPG2386',
+        'CPG2337',
+        'CPG3699',
+        'CPG786',
+        'CPG927',
+        'CPG3244',
+        'CPG2634',
+        'CPG2311',
+        'HG01628',
+        'NA12892',
+        'HG01694',
+        'HG01695',
+        'HG01669',
+        'HG01527',
+        'HG01630',
+        'CPG10066',
+        'CPG1412',
+        'CPG1677',
+        'CPG1784',
+        'CPG1842',
+        'CPG2329',
+        'CPG2691',
+        'CPG3350',
+        'CPG3467',
+        'CPG4374',
+        'CPG6122',
+        'CPG8250',
+        'CPG9191',
+        'CPG9704',
+        'CPG8532',
+        'CPG1099',
+        'CPG3822',
+        'CPG2519',
+        'HG01697',
+        'HG01757',
+        'HG01704',
+        'v3.1::HG02232',
+        'HG01668',
+        'HG01612',
+        'HG01766',
+        'HG01506',
+        'CPG7633',
+        'CPG1248',
+        'CPG1347',
+        'CPG3731',
+        'HG01626',
+        'HG01500',
+        'HG01619',
+        'HGDP01384',
+        'HG01767',
+        'HG01525',
+        'CPG554',
+        'CPG4994',
+        'CPG5066',
+        'CPG67264',
+        'CPG67504',
+        'CPG1305',
+        'CPG232',
+        'CPG208',
+        'CPG67686',
+        'CPG273',
+        'HG01699',
+        'HGDP01396',
+        'HG01610',
+        'HG01509',
+    ]
+
+    mt = mt.filter_cols(hl.literal(outliers).contains(mt.s), keep=False)
+
+    # Remove related samples at the 2nd degree or closer, as indicated by gnomAD
+    mt = mt.filter_cols(mt.hgdp_1kg_metadata.gnomad_release | mt.s.startswith('CPG'))
+
+    # Perform PCA
+    eigenvalues_path = output_path('eigenvalues.ht')
+    scores_path = output_path('scores.ht')
+    loadings_path = output_path('loadings.ht')
+    eigenvalues, scores, loadings = hl.hwe_normalized_pca(
+        mt.GT, compute_loadings=True, k=20
+    )
+    hl.Table.from_pandas(pd.DataFrame(eigenvalues)).export(eigenvalues_path)
+    scores.write(scores_path, overwrite=True)
+    loadings.write(loadings_path, overwrite=True)
+
+
+if __name__ == '__main__':
+    query()
