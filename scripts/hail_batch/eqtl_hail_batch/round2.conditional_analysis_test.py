0d23f528cc394734ce39a9c8bc3022b488585c20
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2021-12-01 15:19:21 +1100

    Add `drop=True` when resetting index

--- scripts/hail_batch/eqtl_hail_batch/round2.conditional_analysis_test.py
+++ scripts/hail_batch/eqtl_hail_batch/round2.conditional_analysis_test.py
@@ -135,7 +135,7 @@ def run_computation_in_scatter(
         significant_snps.sort_values(['geneid', 'FDR'], ascending=True)
         .groupby('geneid')
         .apply(lambda group: group.iloc[1:, 1:])
-        .reset_index()
+        .reset_index(drop=True)
     )
 
     sample_ids = residual_df.loc[:, ['sampleid']]
