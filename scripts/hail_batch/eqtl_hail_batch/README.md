a2bd5092efee4a1c799bc367571b146d8c7859ed
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-01-06 11:45:20 +1100

    Parameterise inputs + add CLI (#168)
    * Parameterise inputs
    
    * Convert double quotes to single quotes + fix small issues
    
    * Add README example
    
    * Add text in 'help' messages
    
    I modified some of the text in the 'help' options. I also reassigned `significant-snps` as `significant_snps` for consistency.
    
    * Fix variable names to be consistent across script
    
    * Update clik option names to use underscore consistently
    
    * Update README
    
    * Add driver wrapper around script
    
    * Fix arg preparation
    
    * Try loading resources in a job to avoid memory blowouts
    
    * Clean-up
    
    * Remove unnecessary comments
    
    * Remove commented out line to save residual df
    
    This is unnecessary, as the residual df is saved later in the script
    
    Co-authored-by: Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>

--- scripts/hail_batch/eqtl_hail_batch/README.md
+++ scripts/hail_batch/eqtl_hail_batch/README.md
@@ -4,6 +4,12 @@ This runs a Hail batch script in order to generate a list of eQTLs from scRNA-se
 
 ```sh
 analysis-runner --dataset tob-wgs \
---access-level test --output-dir "kat/v0" \
---description "eqtl batch job" python3 generate_eqtl_spearman.py
+    --access-level test --output-dir "kat/v0" \
+    --description "eqtl batch job" \
+    python3 round2.conditional_analysis_test.py \
+        --output_prefix 'gs://cpg-tob-wgs-test/kat/' \
+        --residuals 'gs://cpg-tob-wgs-test/kat/input/Plasma_chr22_log_residuals.tsv' \
+        --significant_snps 'gs://cpg-tob-wgs-test/kat/correlation_results.csv' \
+        --genotype 'gs://cpg-tob-wgs-test/kat/input/genotype_chr22.tsv' \
+        --test_subset_genes 5 # test with 5 genes only
 ```
--- scripts/hail_batch/eqtl_hail_batch/round2.conditional_analysis_test.py
+++ scripts/hail_batch/eqtl_hail_batch/round2.conditional_analysis_test.py
@@ -1,6 +1,7 @@
 """Perform conditional analysis on SNPs and expression residuals"""
 
 import os
+
 import hail as hl
 import hailtop.batch as hb
 import pandas as pd
@@ -9,45 +10,31 @@ import statsmodels.stats.multitest as multi
 from patsy import dmatrices  # pylint: disable=no-name-in-module
 from scipy.stats import spearmanr
 
-INPUT_BUCKET = 'cpg-tob-wgs-test'
-OUTPUT_BUCKET = 'cpg-tob-wgs-test'
-DRIVER_IMAGE = os.getenv(
-    'DRIVER_IMAGE',
-    'australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:d2a9c316d6d752edb27623542c8a062db4466842-hail-0.2.73.devc6f6f09cec08',  # noqa: E501; pylint: disable=line-too-long
-)
-DEFAULT_RESIDUALS_PATH = f'gs://{INPUT_BUCKET}/kat/input/Plasma_chr22_log_residuals.tsv'
-DEFAULT_SNPS_PATH = f'gs://{INPUT_BUCKET}/kat/correlation_results.csv'
-# HGDP1KG_TOBWGS = (
-#     'gs://cpg-tob-wgs-main-analysis/joint-calling/v7/ancestry/mt_union_hgdp.mt'
-# )
-
-# def get_number_of_scatters():
-#     """get index of total number of genes"""
-#     # Input filenames
-#     residual_df = pd.read_csv(
-#         f'gs://{INPUT_BUCKET}/kat/input/Plasma_chr22_log_residuals.tsv', sep='\t'
-#     )
-#     significant_snps = pd.read_csv(
-#         f'gs://{INPUT_BUCKET}/kat/test.csv', sep=' ', skipinitialspace=True
-#     )
-
-#     # Identify the top eSNP for each eGene and assign remaining to df
-#     esnp1 = (
-#         significant_snps.sort_values(['geneid', 'p.value'], ascending=True)
-#         .groupby('geneid')
-#         .first()
-#         .reset_index()
-#     )
-#     gene_ids = esnp1['geneid'][esnp1['geneid'].isin(residual_df.columns)]
-
-#     return len(gene_ids)
+import click
 
 
-def get_genotype_df(significant_snps, sample_ids):
-    """load genotype df and filter"""
-    genotype_df = pd.read_csv(
-        f'gs://{INPUT_BUCKET}/kat/input/genotype_chr22.tsv', sep='\t'
+DEFAULT_DRIVER_MEMORY = '4G'
+DEFAULT_DRIVER_IMAGE = 'australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:d2a9c316d6d752edb27623542c8a062db4466842-hail-0.2.73.devc6f6f09cec08'  # noqa: E501; pylint: disable=line-too-long
+DRIVER_IMAGE = os.getenv('DRIVER_IMAGE', DEFAULT_DRIVER_IMAGE)
+
+
+def get_number_of_scatters(residual_df, significant_snps_df):
+    """get index of total number of genes"""
+
+    # Identify the top eSNP for each eGene and assign remaining to df
+    esnp1 = (
+        significant_snps_df.sort_values(['geneid', 'p.value'], ascending=True)
+        .groupby('geneid')
+        .first()
+        .reset_index()
     )
+    gene_ids = esnp1['geneid'][esnp1['geneid'].isin(residual_df.columns)]
+
+    return len(gene_ids)
+
+
+def get_genotype_df(genotype_df, significant_snps_df, sample_ids):
+    """load genotype df and filter"""
     # Subset genotype file for the significant SNPs
     genotype_df = genotype_df.loc[
         genotype_df['sampleid'].isin(  # pylint: disable=unsubscriptable-object
@@ -56,24 +43,20 @@ def get_genotype_df(significant_snps, sample_ids):
         :,
     ]
     genotype_sampleid = genotype_df['sampleid']
-    genotype_df = genotype_df.loc[:, genotype_df.columns.isin(significant_snps.snpid)]
+    genotype_df = genotype_df.loc[
+        :, genotype_df.columns.isin(significant_snps_df.snpid)
+    ]
     genotype_df = genotype_df.assign(sampleid=genotype_sampleid)
 
     return genotype_df
 
 
-def calculate_residual_df(residual_df, significant_snps):
+def calculate_residual_df(genotype_df, residual_df, significant_snps_df):
     """calculate residuals for gene list"""
-    if residual_df is None:
-        residual_df = pd.read_csv(DEFAULT_RESIDUALS_PATH, sep='\t')
-    if significant_snps is None:
-        significant_snps = pd.read_csv(
-            DEFAULT_SNPS_PATH, sep=' ', skipinitialspace=True
-        )
 
     # Identify the top eSNP for each eGene and assign remaining to df
     esnp1 = (
-        significant_snps.sort_values(['geneid', 'FDR'], ascending=True)
+        significant_snps_df.sort_values(['geneid', 'FDR'], ascending=True)
         .groupby('geneid')
         .first()
         .reset_index()
@@ -84,7 +67,7 @@ def calculate_residual_df(residual_df, significant_snps):
     gene_ids = esnp1['geneid'][esnp1['geneid'].isin(residual_df.columns)]
     residual_df = residual_df.loc[:, residual_df.columns.isin(gene_ids)]
     residual_df['sampleid'] = sample_ids
-    genotype_df = get_genotype_df(significant_snps, sample_ids)
+    genotype_df = get_genotype_df(genotype_df, significant_snps_df, sample_ids)
 
     # Find residuals after adjustment of lead SNP
     def calculate_adjusted_residuals(gene_id):
@@ -110,45 +93,38 @@ def calculate_residual_df(residual_df, significant_snps):
     ).T
     adjusted_residual_mat.columns = gene_ids
     adjusted_residual_mat.insert(loc=0, column='sampleid', value=sample_ids.sampleid)
-    adjusted_residual_mat.to_csv(
-        f'gs://{OUTPUT_BUCKET}/kat/eSNP1_adjusted_residuals_{iteration}.tsv'
-    )
 
     return adjusted_residual_mat
 
 
-# Run Spearman rank in parallel by sending genes in a batches
+# Run Spearman rank in parallel by sending genes in batches
 def run_computation_in_scatter(
     iteration,  # pylint: disable=redefined-outer-name
     idx,
+    genotype_df,
     residual_df,
-    significant_snps=None,
+    significant_snps_df,
 ):
     """Run genes in scatter"""
-    # Input filenames
-    if significant_snps is None:
-        significant_snps = pd.read_csv(
-            DEFAULT_SNPS_PATH, sep=' ', skipinitialspace=True
-        )
 
     print(f'iteration = {iteration}')
     print(f'idx = {idx}')
+
     # make sure 'geneid' is the first column
     # otherwise, error thrown when using reset_index
-    cols = list(significant_snps)
+    cols = list(significant_snps_df)
     cols.insert(0, cols.pop(cols.index('geneid')))
-    significant_snps = significant_snps.loc[:, cols]
+    significant_snps_df = significant_snps_df.loc[:, cols]
     esnps_to_test = (
-        significant_snps.sort_values(['geneid', 'FDR'], ascending=True)
+        significant_snps_df.sort_values(['geneid', 'FDR'], ascending=True)
         .groupby('geneid')
         .apply(lambda group: group.iloc[1:, 1:])
         .reset_index()
     )
 
     sample_ids = residual_df.loc[:, ['sampleid']]
-    genotype_df = get_genotype_df(significant_snps, sample_ids)
+    genotype_df = get_genotype_df(genotype_df, significant_snps_df, sample_ids)
 
-    # Spearman's rank correlation
     def spearman_correlation(df):
         """get Spearman rank correlation"""
         gene = df.geneid
@@ -161,7 +137,7 @@ def run_computation_in_scatter(
         return (gene, snp, coef, p)
 
     esnp1 = (
-        significant_snps.sort_values(['geneid', 'FDR'], ascending=True)
+        significant_snps_df.sort_values(['geneid', 'FDR'], ascending=True)
         .groupby('geneid')
         .first()
         .reset_index()
@@ -181,19 +157,15 @@ def run_computation_in_scatter(
     locus = adjusted_spearman_df.snpid.str.split('_', expand=True)[0]
     chromosome = locus.str.split(':', expand=True)[0]
     position = locus.str.split(':', expand=True)[1]
-    (
-        adjusted_spearman_df['locus'],
-        adjusted_spearman_df['chromosome'],
-        adjusted_spearman_df['position'],
-    ) = [
-        locus,
-        chromosome,
-        position,
-    ]
+
+    adjusted_spearman_df['locus'] = locus
+    adjusted_spearman_df['chromosome'] = chromosome
+    adjusted_spearman_df['position'] = position
     adjusted_spearman_df['round'] = iteration
+
     # convert to hail table. Can't call `hl.from_pandas(spearman_df)` directly
     # because it doesnt' work with the spark local backend
-    adjusted_spearman_df.to_csv(f'adjusted_spearman_df.csv')
+    adjusted_spearman_df.to_csv('adjusted_spearman_df.csv')
     hl.init(default_reference='GRCh37')
     t = hl.import_table(
         'adjusted_spearman_df.csv',
@@ -227,9 +199,9 @@ def run_computation_in_scatter(
     adjusted_spearman_df = pd.read_csv('adjusted_spearman_df_annotated.tsv', sep='\t')
 
     # set variables for next iteration of loop
-    significant_snps = adjusted_spearman_df
+    significant_snps_df = adjusted_spearman_df
 
-    return significant_snps
+    return significant_snps_df
 
 
 def merge_significant_snps_dfs(*df_list):
@@ -237,7 +209,6 @@ def merge_significant_snps_dfs(*df_list):
     Merge list of list of sig_snps dataframes
     """
 
-    # merged sig_snps
     merged_sig_snps = pd.concat(df_list)
     pvalues = merged_sig_snps['p.value']
     fdr_values = pd.DataFrame(list(multi.fdrcorrection(pvalues))).iloc[1]
@@ -248,63 +219,130 @@ def merge_significant_snps_dfs(*df_list):
     return merged_sig_snps
 
 
-def convert_dataframe_to_text(df):
+def convert_dataframe_to_text(dataframe):
     """
-    convert to string
+    convert to string for writing
     """
-    return df.to_string()
+    return dataframe.to_string()
 
 
-backend = hb.ServiceBackend(billing_project='tob-wgs', bucket='cpg-tob-wgs-test')
-b = hb.Batch(name='eQTL', backend=backend, default_python_image=DRIVER_IMAGE)
+# Create click command line to enter dependency files
+@click.command()
+@click.option(
+    '--significant_snps', required=True, help='A space separated list of SNPs'
+)
+@click.option('--residuals', required=True, help='A TSV of gene residuals')
+@click.option('--genotype', required=True, help='A TSV of genotypes for each sample')
+@click.option(
+    '--output_prefix',
+    required=True,
+    help='A path prefix of where to output files, eg: gs://MyBucket/output-folder/',
+)
+@click.option(
+    '--iterations', type=int, default=5, help='Number of iterations to perform'
+)
+@click.option(
+    '--test_subset_genes',  # pylint: disable=too-many-locals
+    type=int,
+    help='Test with {test_subset_genes} genes, often = 5.',
+)
+def main(
+    significant_snps: str,
+    residuals,
+    genotype,
+    output_prefix: str,
+    iterations=5,
+    test_subset_genes=None,
+):
+    """
+    Creates a Hail Batch pipeline for calculating EQTL using {iterations} iterations,
+    scattered across the number of genes
+    """
+    dataset = os.getenv('DATASET')
+    access_level = os.getenv('ACCESS_LEVEL')
+    backend = hb.ServiceBackend(
+        billing_project=dataset, bucket=f'cpg-{dataset}-{access_level}'
+    )
+    batch = hb.Batch(name='eQTL', backend=backend, default_python_image=DRIVER_IMAGE)
 
-N_GENES = 5
-# N_GENES = get_number_of_scatters()
-# for i in range(get_number_of_scatters()):
+    if test_subset_genes:
+        n_genes = test_subset_genes
+    else:
+        # load these literally to do the get_number of scatters
+        print(f'Loading residuals: {residuals}')
+        residual_df_literal = pd.read_csv(residuals, sep='\t')
+        print(f'Loading significant_snps: {significant_snps}')
+        significant_snps_df_literal = pd.read_csv(
+            significant_snps, sep=' ', skipinitialspace=True
+        )
 
-previous_sig_snps_result = None  # pylint: disable=invalid-name
-previous_residual_result = None  # pylint: disable=invalid-name
-for iteration in range(5):
+        print('Loaded data to prepare workflow')
+        # test with 5 genes
+        n_genes = test_subset_genes or get_number_of_scatters(
+            residual_df_literal, significant_snps_df_literal
+        )
 
-    calc_resid_df_job = b.new_python_job(f'calculate-resid-df-iter-{iteration}')
-    previous_residual_result = calc_resid_df_job.call(
-        calculate_residual_df, previous_residual_result, previous_sig_snps_result
+    # load these in a python job to avoid memory issues during a submission
+    load_job = batch.new_python_job('load-data')
+    genotype_df = load_job.call(pd.read_csv, genotype, sep='\t')
+    residual_df = load_job.call(pd.read_csv, residuals, sep='\t')
+    significant_snps_df = load_job.call(
+        pd.read_csv, significant_snps, sep=' ', skipinitialspace=True
     )
 
-    # convert residual df to string for output
-    residual_as_str = calc_resid_df_job.call(
-        convert_dataframe_to_text, previous_residual_result
-    )
-    # output residual df for each iteration
-    b.write_output(
-        residual_as_str.as_str(),
-        f'gs://{OUTPUT_BUCKET}/kat/round{iteration+1}_residual_results.csv',  # noqa: E501; pylint: disable=line-too-long
-    )
+    previous_sig_snps_result = significant_snps_df  # pylint: disable=invalid-name
+    previous_residual_result = residual_df  # pylint: disable=invalid-name
+    for iteration in range(iterations):
 
-    sig_snps_dfs = []
-    for i in range(N_GENES):
-        j = b.new_python_job(name=f'process_iter_{iteration}_job_{i}')
-        gene_result: hb.resource.PythonResult = j.call(
-            run_computation_in_scatter,
-            iteration,
-            i,
+        calc_resid_df_job = batch.new_python_job(f'calculate-resid-df-iter-{iteration}')
+        previous_residual_result = calc_resid_df_job.call(
+            calculate_residual_df,
+            genotype_df,
             previous_residual_result,
             previous_sig_snps_result,
         )
-        sig_snps_dfs.append(gene_result)
 
-    merge_job = b.new_python_job(name='merge_scatters')
-    previous_sig_snps_result = merge_job.call(merge_significant_snps_dfs, *sig_snps_dfs)
+        # convert residual df to string for output
+        residual_as_str = calc_resid_df_job.call(
+            convert_dataframe_to_text, previous_residual_result
+        )
+        # output residual df for each iteration
+        batch.write_output(
+            residual_as_str.as_str(),
+            os.path.join(output_prefix, f'round{iteration+1}_residual_results.csv'),
+        )
 
-    # convert sig snps to string for output
-    sig_snps_as_string = merge_job.call(
-        convert_dataframe_to_text, previous_sig_snps_result
-    )
-    # output sig snps for each iteration
-    b.write_output(
-        sig_snps_as_string.as_str(),
-        f'gs://{OUTPUT_BUCKET}/kat/round{iteration+1}_significant_correlation_results.csv',  # noqa: E501; pylint: disable=line-too-long
-    )
+        sig_snps_dfs = []
+        for gene_idx in range(n_genes):
+            j = batch.new_python_job(name=f'process_iter_{iteration}_job_{gene_idx}')
+            gene_result: hb.resource.PythonResult = j.call(
+                run_computation_in_scatter,
+                iteration,
+                gene_idx,
+                genotype_df,
+                previous_residual_result,
+                previous_sig_snps_result,
+            )
+            sig_snps_dfs.append(gene_result)
+
+        merge_job = batch.new_python_job(name='merge_scatters')
+        previous_sig_snps_result = merge_job.call(
+            merge_significant_snps_dfs, *sig_snps_dfs
+        )
+
+        # convert sig snps to string for output
+        sig_snps_as_string = merge_job.call(
+            convert_dataframe_to_text, previous_sig_snps_result
+        )
+        # output sig snps for each iteration
+        sig_snps_output_path = os.path.join(
+            output_prefix, f'round{iteration+1}_significant_correlation_results.csv'
+        )
+        batch.write_output(sig_snps_as_string.as_str(), sig_snps_output_path)
+
+    batch.run(wait=False)
 
 
-b.run(wait=False)
+if __name__ == '__main__':
+    # pylint: disable=no-value-for-parameter
+    main()
