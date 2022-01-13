5f026e4f016c1489a4a3447c84f3107bf9f595f5
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-13 15:51:56 +1100

    first commit

---
+++ scripts/hail_batch/calulate_ld/calculate_ld.py
@@ -0,0 +1,21 @@
+"""Calculate ld using the ld_matrix function"""
+
+import hail as hl
+
+TOB_WGS = 'gs://cpg-tob-wgs-test/mt/v7.mt/'
+
+
+def query():
+    """Query script entry point."""
+
+    hl.init(default_reference='GRCh38')
+
+    tob_wgs = hl.read_matrix_table(TOB_WGS)
+    tob_wgs = hl.experimental.densify(tob_wgs)
+    print('Starting ld calculation')
+    ld = hl.ld_matrix(mt.GT.n_alt_alleles(), mt.locus, radius=1e6)
+    ld.to_numpy()
+
+
+if __name__ == '__main__':
+    query()
