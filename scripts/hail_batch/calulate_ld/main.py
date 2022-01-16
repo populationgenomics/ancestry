c5f072032e99ba327f1cf6d0b733bfbebe502030
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-17 10:39:12 +1100

    Update incorrect name from 'calculate_maf' to 'calculate_ld'

--- scripts/hail_batch/calulate_ld/main.py
+++ scripts/hail_batch/calulate_ld/main.py
@@ -5,18 +5,18 @@ import hailtop.batch as hb
 from analysis_runner import dataproc
 
 service_backend = hb.ServiceBackend(
-    billing_project=os.getenv("HAIL_BILLING_PROJECT"), bucket=os.getenv("HAIL_BUCKET")
+    billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
 )
 
-batch = hb.Batch(name="calculate-ld", backend=service_backend)
+batch = hb.Batch(name='calculate-ld', backend=service_backend)
 
 dataproc.hail_dataproc_job(
     batch,
-    f"calculate_maf.py",
-    max_age="4h",
+    f'calculate_ld.py',
+    max_age='4h',
     num_secondary_workers=20,
-    init=["gs://cpg-reference/hail_dataproc/install_common.sh"],
-    job_name=f"calculate_ld",
+    init=['gs://cpg-reference/hail_dataproc/install_common.sh'],
+    job_name=f'calculate_ld',
     worker_boot_disk_size=200,
 )
 
