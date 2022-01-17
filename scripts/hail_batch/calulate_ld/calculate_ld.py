702e5f87b0dd03d83ed3024509b4ea5e7ffab359
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-17 15:39:19 +1100

    Subset to first 100k filtered rows

--- scripts/hail_batch/calulate_ld/calculate_ld.py
+++ scripts/hail_batch/calulate_ld/calculate_ld.py
@@ -15,6 +15,7 @@ def query():
     tob_wgs = hl.experimental.densify(tob_wgs)
     # filter out constant variants
     tob_wgs = tob_wgs.filter_rows(hl.len(tob_wgs.alleles) == 2)
+    tob_wgs = tob_wgs.head(100000)
     ld = hl.ld_matrix(tob_wgs.GT.n_alt_alleles(), tob_wgs.locus, radius=2e6)
     BlockMatrix.write(ld, 'gs://cpg-tob-wgs-test/kat/v0/ld_matrix.bm')
 
