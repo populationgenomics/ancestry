442aa99f26187dd99e92fb7e69f3cb01d518624c
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-18 10:47:11 +1100

    Save output of ld summary stats

--- scripts/hail_batch/calulate_ld/calculate_ld.py
+++ scripts/hail_batch/calulate_ld/calculate_ld.py
@@ -1,7 +1,7 @@
 """Calculate ld using the ld_matrix function"""
 
 import hail as hl
-from hail.linalg import BlockMatrix
+import pandas as pd
 
 TOB_WGS = 'gs://cpg-tob-wgs-test/mt/v7.mt/'
 
@@ -16,8 +16,21 @@ def query():
     # filter out constant variants
     tob_wgs = tob_wgs.filter_rows(hl.len(tob_wgs.alleles) == 2)
     tob_wgs = tob_wgs.head(100000)
-    ld = hl.ld_matrix(tob_wgs.GT.n_alt_alleles(), tob_wgs.locus, radius=1e6)
-    BlockMatrix.write(ld, 'gs://cpg-tob-wgs-test/kat/v1/ld_matrix.bm')
+    ld = hl.ld_matrix(tob_wgs.GT.n_alt_alleles(), tob_wgs.locus, radius=2e6)
+    ld = pd.DataFrame(ld.to_numpy())
+    # save pandas ld dataframe
+    ld.to_csv('gs://cpg-tob-wgs-test/kat/v0/ld_matrix_2M_radius.csv')
+    # get number of non-zero, non na values across rows
+    nonzero_values = ld.fillna(0).astype(bool).sum(axis=1)
+    nonzero_values.to_csv(
+        'gs://cpg-tob-wgs-test/kat/v0/nonzero_nona_values_2M_radius.csv'
+    )
+    # get number of positive values (including 1's) across rows
+    positive_values = ld.fillna(0).gt(0).sum(axis=1)
+    positive_values.to_csv('gs://cpg-tob-wgs-test/kat/v0/positive_values_2M_radius.csv')
+    # get number of negative values across rows
+    negative_values = ld.fillna(0).lt(0).sum(axis=1)
+    negative_values.to_csv('gs://cpg-tob-wgs-test/kat/v0/negative_values_2M_radius.csv')
 
 
 if __name__ == '__main__':
