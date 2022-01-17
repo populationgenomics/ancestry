6d4d7e0722a943ade5a448b014ef8b15de559aa1
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-17 16:55:12 +1100

    Test radius of 1M

--- scripts/hail_batch/calulate_ld/calculate_ld.py
+++ scripts/hail_batch/calulate_ld/calculate_ld.py
@@ -16,8 +16,8 @@ def query():
     # filter out constant variants
     tob_wgs = tob_wgs.filter_rows(hl.len(tob_wgs.alleles) == 2)
     tob_wgs = tob_wgs.head(100000)
-    ld = hl.ld_matrix(tob_wgs.GT.n_alt_alleles(), tob_wgs.locus, radius=2e6)
-    BlockMatrix.write(ld, 'gs://cpg-tob-wgs-test/kat/v0/ld_matrix.bm')
+    ld = hl.ld_matrix(tob_wgs.GT.n_alt_alleles(), tob_wgs.locus, radius=1e6)
+    BlockMatrix.write(ld, 'gs://cpg-tob-wgs-test/kat/v1/ld_matrix.bm')
 
 
 if __name__ == '__main__':
