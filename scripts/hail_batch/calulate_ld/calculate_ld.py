fb11eeda8c6bebebcdba340ba18f2836638eaf26
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-18 15:41:55 +1100

    Test ld_matrix on 40k loci

--- scripts/hail_batch/calulate_ld/calculate_ld.py
+++ scripts/hail_batch/calulate_ld/calculate_ld.py
@@ -15,25 +15,26 @@ def query():
     tob_wgs = hl.experimental.densify(tob_wgs)
     # filter out constant variants
     tob_wgs = tob_wgs.filter_rows(hl.len(tob_wgs.alleles) == 2)
-    tob_wgs = tob_wgs.head(30000)
+    tob_wgs = tob_wgs.head(40000)
     ld = hl.ld_matrix(tob_wgs.GT.n_alt_alleles(), tob_wgs.locus, radius=2e6)
     ld = ld.to_numpy()
-    # save numpy array
     ld = pd.DataFrame(ld)
+    # save pandas df
+    ld.to_csv('gs://cpg-tob-wgs-test/kat/v1/ld_matrix_40k.csv')
     # get number of non-zero, non na values across rows
     nonzero_values = ld.fillna(0).astype(bool).sum(axis=1)
     nonzero_values.to_csv(
-        'gs://cpg-tob-wgs-test/kat/v1/nonzero_nona_values_2M_radius_30k.csv'
+        'gs://cpg-tob-wgs-test/kat/v1/nonzero_nona_values_2M_radius_40k.csv'
     )
     # get number of positive values (including 1's) across rows
     positive_values = ld.fillna(0).gt(0).sum(axis=1)
     positive_values.to_csv(
-        'gs://cpg-tob-wgs-test/kat/v1/positive_values_2M_radius_30k.csv'
+        'gs://cpg-tob-wgs-test/kat/v1/positive_values_2M_radius_40k.csv'
     )
     # get number of negative values across rows
     negative_values = ld.fillna(0).lt(0).sum(axis=1)
     negative_values.to_csv(
-        'gs://cpg-tob-wgs-test/kat/v1/negative_values_2M_radius_30k.csv'
+        'gs://cpg-tob-wgs-test/kat/v1/negative_values_2M_radius_40k.csv'
     )
 
 
