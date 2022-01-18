41cb114862be5d1504419dd4e849754dd22c5b0d
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-18 14:47:31 +1100

    Debug 'ValueError: cannot reshape array of size 0 into shape' by saving numpy array as intermediate

--- scripts/hail_batch/calulate_ld/calculate_ld.py
+++ scripts/hail_batch/calulate_ld/calculate_ld.py
@@ -2,6 +2,7 @@
 
 import hail as hl
 import pandas as pd
+import numpy as np
 
 TOB_WGS = 'gs://cpg-tob-wgs-test/mt/v7.mt/'
 
@@ -17,24 +18,25 @@ def query():
     tob_wgs = tob_wgs.filter_rows(hl.len(tob_wgs.alleles) == 2)
     tob_wgs = tob_wgs.head(50000)
     ld = hl.ld_matrix(tob_wgs.GT.n_alt_alleles(), tob_wgs.locus, radius=2e6)
-    ld = pd.DataFrame(ld.to_numpy())
-    # save pandas ld dataframe
-    ld.to_csv('gs://cpg-tob-wgs-test/kat/v1/ld_matrix_2M_radius_50k.csv')
-    # get number of non-zero, non na values across rows
-    nonzero_values = ld.fillna(0).astype(bool).sum(axis=1)
-    nonzero_values.to_csv(
-        'gs://cpg-tob-wgs-test/kat/v1/nonzero_nona_values_2M_radius_50k.csv'
-    )
-    # get number of positive values (including 1's) across rows
-    positive_values = ld.fillna(0).gt(0).sum(axis=1)
-    positive_values.to_csv(
-        'gs://cpg-tob-wgs-test/kat/v1/positive_values_2M_radius_50k.csv'
-    )
-    # get number of negative values across rows
-    negative_values = ld.fillna(0).lt(0).sum(axis=1)
-    negative_values.to_csv(
-        'gs://cpg-tob-wgs-test/kat/v1/negative_values_2M_radius_50k.csv'
-    )
+    ld = ld.to_numpy()
+    # save numpy array
+    np.save('gs://cpg-tob-wgs-test/kat/v1/np_array_2M_radius_50k.csv', ld)
+    ld = pd.DataFrame(ld)
+    # # get number of non-zero, non na values across rows
+    # nonzero_values = ld.fillna(0).astype(bool).sum(axis=1)
+    # nonzero_values.to_csv(
+    #     'gs://cpg-tob-wgs-test/kat/v1/nonzero_nona_values_2M_radius_50k.csv'
+    # )
+    # # get number of positive values (including 1's) across rows
+    # positive_values = ld.fillna(0).gt(0).sum(axis=1)
+    # positive_values.to_csv(
+    #     'gs://cpg-tob-wgs-test/kat/v1/positive_values_2M_radius_50k.csv'
+    # )
+    # # get number of negative values across rows
+    # negative_values = ld.fillna(0).lt(0).sum(axis=1)
+    # negative_values.to_csv(
+    #     'gs://cpg-tob-wgs-test/kat/v1/negative_values_2M_radius_50k.csv'
+    # )
 
 
 if __name__ == '__main__':
