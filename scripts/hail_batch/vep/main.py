5da81867a6d029a9d3e7732cd424b37526da125e
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-19 16:18:22 +1100

    Save vep output

--- scripts/hail_batch/vep/main.py
+++ scripts/hail_batch/vep/main.py
@@ -8,20 +8,23 @@ python script to try and run vep
 
 import click
 import hail as hl
+from analysis_runner import bucket_path, output_path
 
-TOB_WGS = 'gs://cpg-tob-wgs-test/mt/v7.mt/'
+TOB_WGS = bucket_path('mt/v7.mt/')
 
 
 @click.command()
 def main():
     """
-    just runs vep
+    Run vep using run_vep.py wrapper
     """
 
     hl.init(default_reference='GRCh38')
 
     tob_wgs = hl.read_matrix_table(TOB_WGS)
-    hl.vep(tob_wgs)
+    vep = hl.vep(tob_wgs)
+    vep_filename = output_path(f'tobwgs_v7_vep.mt')
+    vep.write(vep_filename)
 
 
 if __name__ == '__main__':
