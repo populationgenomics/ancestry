91ed3eb9aa8f64fece6cfcfc4301ce1e5b432f98
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-20 13:56:11 +1100

    Run VEP on full TOB dataset

--- scripts/hail_batch/vep/only_vep.py
+++ scripts/hail_batch/vep/only_vep.py
@@ -23,9 +23,8 @@ def main():
     tob_wgs = hl.read_matrix_table(TOB_WGS)
     # filter to biallelic loci only
     tob_wgs = tob_wgs.filter_rows(hl.len(tob_wgs.alleles) == 2)
-    tob_wgs = tob_wgs.head(10)
     vep = hl.vep(tob_wgs)
-    vep_filename = 'gs://cpg-tob-wgs-test/kat/v0/tobwgs_v7_vep.mt'
+    vep_filename = 'gs://cpg-tob-wgs-test/kat/v0/tobwgs_v7_vep_full.mt'
     vep.write(vep_filename)
 
 
