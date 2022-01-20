ddcd93999957627d679bf11959df1c9399d4cd0c
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-20 15:17:49 +1100

    Remove variants with *, as VEP is not able to handle these

--- scripts/hail_batch/vep/only_vep.py
+++ scripts/hail_batch/vep/only_vep.py
@@ -23,6 +23,7 @@ def main():
     tob_wgs = hl.read_matrix_table(TOB_WGS)
     # filter to biallelic loci only
     tob_wgs = tob_wgs.filter_rows(hl.len(tob_wgs.alleles) == 2)
+    tob_wgs = tob_wgs.filter_rows(tob_wgs.alleles[1] != '*')
     vep = hl.vep(tob_wgs)
     vep_filename = 'gs://cpg-tob-wgs-test/kat/v0/tobwgs_v7_vep_full.mt'
     vep.write(vep_filename)
