141435d5488900c0b0cddb65d52f1f1d7cda2659
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-20 12:41:06 +1100

    Save block matrix only after calculating LD

--- scripts/hail_batch/calculate_ld/calculate_ld.py
+++ scripts/hail_batch/calculate_ld/calculate_ld.py
@@ -1,8 +1,7 @@
 """Calculate ld using the ld_matrix function"""
 
 import hail as hl
-import pandas as pd
-from analysis_runner import bucket_path, output_path
+from analysis_runner import bucket_path
 
 TOB_WGS = bucket_path('mt/v7.mt/')
 
@@ -16,12 +15,10 @@ def query():
     tob_wgs = hl.experimental.densify(tob_wgs)
     # filter out constant variants
     tob_wgs = tob_wgs.filter_rows(hl.len(tob_wgs.alleles) == 2)
-    tob_wgs = tob_wgs.head(30000)
+    tob_wgs = tob_wgs.head(50000)
     ld = hl.ld_matrix(tob_wgs.GT.n_alt_alleles(), tob_wgs.locus, radius=1e6)
-    ld = pd.DataFrame(ld.to_numpy())
-    # save pandas df
-    ld_filename = output_path(f'ld_matrix.csv', 'analysis')
-    ld.to_csv(ld_filename, index=False)
+    # save block matrix
+    ld.write('gs://cpg-tob-wgs-test/kat/v0/ld_matrix_1M_50k.bm')
 
 
 if __name__ == '__main__':
