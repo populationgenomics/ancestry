959c408b0566cefb6e7a5e50bf1b3f5d4045289a
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-21 14:15:20 +1100

    Test running `ld_matrix` on entire dataset

--- scripts/hail_batch/calculate_ld/calculate_ld.py
+++ scripts/hail_batch/calculate_ld/calculate_ld.py
@@ -14,15 +14,11 @@ def query():
     tob_wgs = hl.read_matrix_table(TOB_WGS)
     tob_wgs = hl.experimental.densify(tob_wgs)
     # filter out constant variants
-    tob_wgs = tob_wgs.filter_rows(hl.len(tob_wgs.alleles) == 2)
-    tob_wgs = tob_wgs.head(50000)
+    tob_wgs = tob_wgs.annotate_rows(stats=hl.agg.stats(tob_wgs.GT.n_alt_alleles()))
+    tob_wgs = tob_wgs.filter_rows(tob_wgs.stats.stdev != 0)
     ld = hl.ld_matrix(tob_wgs.GT.n_alt_alleles(), tob_wgs.locus, radius=2e6)
     # save block matrix
-    ld.write('gs://cpg-tob-wgs-test/kat/v0/ld_matrix_2M_50k.bm')
-
-
-if __name__ == '__main__':
-    query()
+    ld.write('gs://cpg-tob-wgs-test/kat/v0/ld_matrix_2Mradius_full.bm')
 
 
 if __name__ == '__main__':
