5e9827ed16137a34582146f28ad3319af7955f7d
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-24 11:55:34 +1100

    Test VEP on GRCh37

--- scripts/hail_batch/vep/only_vep.py
+++ scripts/hail_batch/vep/only_vep.py
@@ -9,7 +9,7 @@ python script to try and run vep
 import click
 import hail as hl
 
-TOB_WGS = 'gs://cpg-tob-wgs-test/mt/v7.mt/'
+TOB_WGS = 'gs://cpg-tob-wgs-test/mt/mt37.mt/'
 
 
 @click.command()
@@ -18,14 +18,14 @@ def main():
     Run vep using run_vep.py wrapper
     """
 
-    hl.init(default_reference='GRCh38')
+    hl.init(default_reference='GRCh37')
 
     tob_wgs = hl.read_matrix_table(TOB_WGS)
     # filter to biallelic loci only
     tob_wgs = tob_wgs.filter_rows(hl.len(tob_wgs.alleles) == 2)
     tob_wgs = tob_wgs.filter_rows(tob_wgs.alleles[1] != '*')
-    vep = hl.vep(tob_wgs)
-    vep_filename = 'gs://cpg-tob-wgs-test/kat/v0/tobwgs_v7_vep_full.mt'
+    vep = hl.vep(tob_wgs, 'gs://hail-us-vep/vep85-loftee-gcloud.json')
+    vep_filename = 'gs://cpg-tob-wgs-test/kat/v0/tobwgs_v7_vep_grch37.mt'
     vep.write(vep_filename)
 
 
