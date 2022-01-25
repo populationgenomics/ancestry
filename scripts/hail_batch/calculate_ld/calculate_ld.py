13352c99505c7bb98d3ba9076d9f69cedafa61ef
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-01-25 12:09:38 +1100

    Test ld and filtering entire block matrix

--- scripts/hail_batch/calculate_ld/calculate_ld.py
+++ scripts/hail_batch/calculate_ld/calculate_ld.py
@@ -1,11 +1,9 @@
 """Calculate ld using the ld_matrix function"""
 
 import hail as hl
+from analysis_runner import bucket_path
 
-# from analysis_runner import bucket_path
-from hail.linalg import BlockMatrix
-
-# TOB_WGS = bucket_path('mt/v7.mt/')
+TOB_WGS = bucket_path('mt/v7.mt/')
 
 
 def query():
@@ -13,18 +11,28 @@ def query():
 
     hl.init(default_reference='GRCh38')
 
-    # tob_wgs = hl.read_matrix_table(TOB_WGS)
-    # tob_wgs = hl.experimental.densify(tob_wgs)
-    # # filter out constant variants
-    # tob_wgs = tob_wgs.annotate_rows(stats=hl.agg.stats(tob_wgs.GT.n_alt_alleles()))
-    # tob_wgs = tob_wgs.filter_rows(tob_wgs.stats.stdev != 0)
-    # ld = hl.ld_matrix(tob_wgs.GT.n_alt_alleles(), tob_wgs.locus, radius=2e6)
-    # # save block matrix
-    # ld.write('gs://cpg-tob-wgs-test/kat/v0/ld_matrix_2Mradius_full.bm')
-    bm = BlockMatrix.read('gs://cpg-tob-wgs-test/kat/v0/ld_matrix_1M_50k.bm/')
-    ht = bm.entries().head(1000)
-    # save block matrix
-    ht.write('gs://cpg-tob-wgs-test/kat/v0/ld_matrix_1k.ht')
+    tob_wgs = hl.read_matrix_table(TOB_WGS)
+    # add in global position and stats for each locus
+    tob_wgs = tob_wgs.annotate_rows(
+        global_position=hl.locus(
+            tob_wgs.locus.contig, tob_wgs.locus.position
+        ).global_position(),
+        stats=hl.agg.stats(tob_wgs.GT.n_alt_alleles()),
+    )
+    tob_wgs = tob_wgs.filter_rows(tob_wgs.stats.stdev != 0)
+    # add row index to be able to remap
+    tob_wgs = tob_wgs.add_row_index()
+    # turn tob matrix into table and save
+    tob_wgs = tob_wgs.key_rows_by('locus', 'alleles', 'row_idx', 'global_position')
+    tob_wgs = tob_wgs.select_rows().select_globals()
+    tob_locus_info = tob_wgs.rows()
+    tob_locus_info.write('gs://cpg-tob-wgs-test/kat/v0/tob_locus_info.ht')
+    ld = hl.ld_matrix(tob_wgs.GT.n_alt_alleles(), tob_wgs.locus, radius=2e6)
+    table = ld.entries()
+    # filter out entries with an LD score less than 0.1
+    table = table.filter(table.entry > 0.1)
+    # save table
+    table.write('gs://cpg-tob-wgs-test/kat/v0/ld_matrix_full_filtered.ht')
 
 
 if __name__ == '__main__':
