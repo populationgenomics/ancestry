eae91f56f4ee2d1424a77ad791a87ba6858f5a48
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-02-02 12:38:00 +1100

    Launch `caclulate_residuals` as separate job and refactor code

--- scripts/hail_batch/eqtl_hail_batch/generate_eqtl_spearman.py
+++ scripts/hail_batch/eqtl_hail_batch/generate_eqtl_spearman.py
@@ -16,9 +16,9 @@ DEFAULT_DRIVER_IMAGE = 'australia-southeast1-docker.pkg.dev/analysis-runner/imag
 DRIVER_IMAGE = os.getenv('DRIVER_IMAGE', DEFAULT_DRIVER_IMAGE)
 
 
-def get_number_of_scatters(expression_df, geneloc_df):
-    """get index of total number of genes"""
-    # Remove genes with 0 expression in all samples
+def filter_lowly_expressed_genes(expression_df):
+    """Remove genes with low expression in all samples"""
+
     expression_df = expression_df.loc[:, (expression_df != 0).any(axis=0)]
     # Number of individuals with non-zero expression
     genes_not_equal_zero = expression_df.iloc[:, 1:].values != 0
@@ -31,49 +31,47 @@ def get_number_of_scatters(expression_df, geneloc_df):
     sample_ids = expression_df['sampleid']
     expression_df = expression_df[atleast10percent.index]
     expression_df.insert(loc=0, column='sampleid', value=sample_ids)
+
+    return expression_df
+
+
+def get_number_of_scatters(expression_df, geneloc_df):
+    """get index of total number of genes"""
+
+    expression_df = filter_lowly_expressed_genes(expression_df)
     gene_ids = list(expression_df.columns.values)[1:]
     geneloc_df = geneloc_df[geneloc_df.geneid.isin(gene_ids)]
 
     return len(geneloc_df.index)
 
 
-# Run Spearman rank in parallel by sending genes in a batches
-def run_spearman_correlation_scatter(
-    idx, expression_df, genotype_df, geneloc_df, snploc_df, covariate_df, output_prefix
-):  # pylint: disable=too-many-locals
-    """Run genes in scatter"""
+def get_log_expression(expression_df):
 
-    # Remove genes with 0 expression in all samples
-    expression_df = expression_df.loc[:, (expression_df != 0).any(axis=0)]
-    # Number of individuals with non-zero expression
-    genes_not_equal_zero = expression_df.iloc[:, 1:].values != 0
-    n_expr_over_zero = pd.DataFrame(genes_not_equal_zero.sum(axis=0))
-    percent_expr_over_zero = (n_expr_over_zero / len(expression_df.index)) * 100
-    percent_expr_over_zero.index = expression_df.columns[1:]
+    """get logged expression values"""
 
-    # Filter genes with less than 10 percent individuals with non-zero expression
-    atleast10percent = percent_expr_over_zero[(percent_expr_over_zero > 10)[0]]
-    sample_ids = expression_df['sampleid']
-    expression_df = expression_df[atleast10percent.index]
-    expression_df.insert(loc=0, column='sampleid', value=sample_ids)
+    expression_df = filter_lowly_expressed_genes(expression_df)
 
-    # Prepare variables used to calculate Spearman's correlation
-    gene_ids = list(expression_df.columns.values)[1:]
+    # Prepare variables
     sample_ids = expression_df.iloc[:, 0]
-    genotype_df = genotype_df[genotype_df.sampleid.isin(expression_df.sampleid)]
 
-    # Get 1Mb sliding window around each gene
-    geneloc_df = geneloc_df[geneloc_df.geneid.isin(gene_ids)]
-    geneloc_df = geneloc_df.assign(left=geneloc_df.start - 1000000)
-    geneloc_df = geneloc_df.assign(right=geneloc_df.end + 1000000)
-    geneloc_df.to_csv(output_prefix + f'_gene_SNP_pairs.tsv')
-
+    # log expression values
     to_log = expression_df.iloc[:, 1:].columns
     log_expression_df = expression_df[to_log].applymap(lambda x: np.log(x + 1))
     log_expression_df.insert(loc=0, column='sampleid', value=sample_ids)
 
+    return log_expression_df
+
+
+def calculate_residuals(expression_df, covariate_df, output_prefix):
+    """Calculate residuals for each gene in scatter"""
+
+    log_expression_df = get_log_expression(expression_df)
+    # Prepare variables
+    gene_ids = list(log_expression_df.columns.values)[1:]
+    sample_ids = log_expression_df.iloc[:, 0]
+
     # Calculate expression residuals
-    def calculate_residuals(gene_id):
+    def calculate_gene_residual(gene_id):
         """Calculate gene residuals"""
         gene = gene_id
         exprs_val = log_expression_df[['sampleid', gene]]
@@ -87,16 +85,42 @@ def run_spearman_correlation_scatter(
         residuals = list(model.fit().resid)
         return residuals
 
-    residual_df = pd.DataFrame(list(map(calculate_residuals, gene_ids))).T
+    residual_df = pd.DataFrame(list(map(calculate_gene_residual, gene_ids))).T
     residual_df.columns = gene_ids
     residual_df = residual_df.assign(sampleid=list(sample_ids))
     residual_df.to_csv(output_prefix + f'_log_residuals.tsv')
+
+    return residual_df
+
+
+# Run Spearman rank in parallel by sending genes in a batches
+def run_spearman_correlation_scatter(
+    idx,
+    expression_df,
+    genotype_df,
+    geneloc_df,
+    snploc_df,
+    residuals_df,
+):  # pylint: disable=too-many-locals
+    """Run genes in scatter"""
+
+    log_expression_df = get_log_expression(expression_df)
+
+    # Prepare variables used to calculate Spearman's correlation
+    gene_ids = list(log_expression_df.columns.values)[1:]
+    genotype_df = genotype_df[genotype_df.sampleid.isin(log_expression_df.sampleid)]
+
+    # Get 1Mb sliding window around each gene
+    geneloc_df = geneloc_df[geneloc_df.geneid.isin(gene_ids)]
+    geneloc_df = geneloc_df.assign(left=geneloc_df.start - 1000000)
+    geneloc_df = geneloc_df.assign(right=geneloc_df.end + 1000000)
+    # geneloc_df.to_csv(output_prefix + f'_gene_SNP_pairs.tsv')
 
     def spearman_correlation(df):
         """get Spearman rank correlation"""
         gene = df.geneid
         snp = df.snpid
-        res_val = residual_df[['sampleid', gene]]
+        res_val = residuals_df[['sampleid', gene]]
         genotype_val = genotype_df[['sampleid', snp]]
         test_df = res_val.merge(genotype_val, on='sampleid', how='left')
         test_df.columns = ['sampleid', 'residual', 'SNP']
@@ -167,7 +191,7 @@ def merge_df_and_convert_to_string(*df_list):
 @click.option(
     '--covariates', required=True, help='A TSV of covariates to calculate residuals'
 )
-@click.option(
+@click.option(  # pylint: disable=too-many-locals
     '--output-prefix',
     required=True,
     help='A path prefix of where to output files, eg: gs://MyBucket/output-folder/',
@@ -201,6 +225,13 @@ def main(
     geneloc_df = load_job.call(pd.read_csv, geneloc, sep='\t')
     snploc_df = load_job.call(pd.read_csv, snploc, sep='\t')
     covariate_df = load_job.call(pd.read_csv, covariates, sep='\t')
+    calculate_residuals_job = batch.new_python_job('load-data')
+    residuals_df = calculate_residuals_job.call(
+        calculate_residuals,
+        expression_df=expression_df,
+        covariate_df=covariate_df,
+        output_prefix=output_prefix,
+    )
 
     spearman_dfs_from_scatter = []
     for idx in range(get_number_of_scatters(expression_df_literal, geneloc_df_literal)):
@@ -208,13 +239,12 @@ def main(
         j = batch.new_python_job(name=f'process_{idx}')
         result: hb.resource.PythonResult = j.call(
             run_spearman_correlation_scatter,
-            idx,
-            expression_df,
-            genotype_df,
-            geneloc_df,
-            snploc_df,
-            covariate_df,
-            output_prefix,
+            idx=idx,
+            expression_df=expression_df,
+            genotype_df=genotype_df,
+            geneloc_df=geneloc_df,
+            snploc_df=snploc_df,
+            residuals_df=residuals_df,
         )
         spearman_dfs_from_scatter.append(result)
 
