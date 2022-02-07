475539a35d0057e4c09481cf3242693628fb3f8f
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-02-07 17:53:54 +1100

    Plot LD variants across the genome

--- scripts/hail_batch/calculate_ld/ld_exploration.py
+++ scripts/hail_batch/calculate_ld/ld_exploration.py
@@ -1,6 +1,9 @@
 """Calculate ld using the ld_matrix function"""
 
 import hail as hl
+from bokeh.resources import CDN
+from bokeh.embed import file_html
+from analysis_runner import output_path
 
 # from analysis_runner import bucket_path
 
@@ -13,13 +16,6 @@ def query():
 
     hl.init(default_reference='GRCh38')
 
-    ht_agg = hl.read_table(
-        'gs://cpg-tob-wgs-test/kat/v0/ld_matrix_aggregated_table02.ht'
-    )
-    min_val = ht_agg.aggregate(hl.agg.min(hl.len(ht_agg.values)))
-    max_val = ht_agg.aggregate(hl.agg.max(hl.len(ht_agg.values)))
-    median = ht_agg.aggregate(hl.agg.approx_quantiles(hl.len(ht_agg.values), 0.5))
-    print(f'The min is {min_val}, the max is {max_val}, and the median is {median}')
     # ht = hl.read_table(LD_MATRIX)
     # annotation = hl.read_table(TOB_LOCUS_INFO).key_by('row_idx')
     # # add in global positions for i
@@ -31,6 +27,26 @@ def query():
     # # collapse ht by variant and save
     # ht_agg = ht.key_by('global_pos_i').drop('i', 'row_idx').collect_by_key()
     # ht_agg.write('gs://cpg-tob-wgs-test/kat/v0/ld_matrix_aggregated_table02.ht')
+    ht_agg = hl.read_table(
+        'gs://cpg-tob-wgs-test/kat/v0/ld_matrix_aggregated_table02.ht'
+    )
+    # min_val = ht_agg.aggregate(hl.agg.min(hl.len(ht_agg.values)))
+    # max_val = ht_agg.aggregate(hl.agg.max(hl.len(ht_agg.values)))
+    # median = ht_agg.aggregate(hl.agg.approx_quantiles(hl.len(ht_agg.values), 0.5))
+    # print(f'The min is {min_val}, the max is {max_val}, and the median is {median}')
+    ht_agg = ht_agg.annotate(n_ld=hl.len(ht_agg.values))
+    plot = hl.plot.scatter(
+        ht_agg.global_pos_i,
+        ht_agg.n_ld,
+        xlabel='Global Position',
+        ylabel='ld variants (n)',
+        title='Linked variants across the genome',
+    )
+    plot_filename_html = output_path(f'ld_variants_across_genome.html', 'web')
+    html = file_html(plot, CDN, 'my plot')
+    plot_filename_html = output_path(f'scree_plot.html', 'web')
+    with hl.hadoop_open(plot_filename_html, 'w') as f:
+        f.write(html)
 
 
 if __name__ == '__main__':
