0a81412ec573d162108815b5733e0998f5a42896
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-02-09 10:32:57 +1100

    Get summary statistics and create manhattan plot for biallelic loci only

--- scripts/hail_batch/calculate_ld/ld_exploration.py
+++ scripts/hail_batch/calculate_ld/ld_exploration.py
@@ -5,10 +5,10 @@ from bokeh.resources import CDN
 from bokeh.embed import file_html
 from analysis_runner import output_path
 
-# from analysis_runner import bucket_path
+from analysis_runner import bucket_path
 
-# LD_MATRIX = bucket_path('kat/v0/ld_matrix_full_filtered02.ht/')
-# TOB_LOCUS_INFO = 'gs://cpg-tob-wgs-test/kat/v0/tob_locus_info.ht/'
+LD_MATRIX = bucket_path('kat/v0/ld_matrix_full_filtered02_biallelic.ht/')
+TOB_LOCUS_INFO = 'gs://cpg-tob-wgs-test/kat/v0/tob_locus_info_biallelic.ht/'
 
 
 def query():
@@ -16,20 +16,22 @@ def query():
 
     hl.init(default_reference='GRCh38')
 
-    # ht = hl.read_table(LD_MATRIX)
-    # annotation = hl.read_table(TOB_LOCUS_INFO).key_by('row_idx')
-    # # add in global positions for i
-    # ht = ht.rename({'i': 'row_idx'}).key_by('row_idx')
-    # ht = ht.annotate(global_pos_i=annotation[ht.row_idx].global_position)
-    # # add in global positions for j
-    # ht = ht.rename({'row_idx': 'i', 'j': 'row_idx'}).key_by('row_idx')
-    # ht = ht.annotate(global_pos_j=annotation[ht.row_idx].global_position)
-    # # collapse ht by variant and save
-    # ht_agg = ht.key_by('global_pos_i').drop('i', 'row_idx').collect_by_key()
-    # ht_agg.write('gs://cpg-tob-wgs-test/kat/v0/ld_matrix_aggregated_table02.ht')
-    ht_agg = hl.read_table(
-        'gs://cpg-tob-wgs-test/kat/v0/ld_matrix_aggregated_table02.ht'
+    ht = hl.read_table(LD_MATRIX)
+    annotation = hl.read_table(TOB_LOCUS_INFO).key_by('row_idx')
+    # add in global positions for i
+    ht = ht.rename({'i': 'row_idx'}).key_by('row_idx')
+    ht = ht.annotate(global_pos_i=annotation[ht.row_idx].global_position)
+    # add in global positions for j
+    ht = ht.rename({'row_idx': 'i', 'j': 'row_idx'}).key_by('row_idx')
+    ht = ht.annotate(global_pos_j=annotation[ht.row_idx].global_position)
+    # collapse ht by variant and save
+    ht_agg = ht.key_by('global_pos_i').drop('i', 'row_idx').collect_by_key()
+    ht_agg.write(
+        'gs://cpg-tob-wgs-test/kat/v0/ld_matrix_aggregated_table02_biallelic.ht'
     )
+    # ht_agg = hl.read_table(
+    #     'gs://cpg-tob-wgs-test/kat/v0/ld_matrix_aggregated_table02.ht'
+    # )
     # min_val = ht_agg.aggregate(hl.agg.min(hl.len(ht_agg.values)))
     # max_val = ht_agg.aggregate(hl.agg.max(hl.len(ht_agg.values)))
     # median = ht_agg.aggregate(hl.agg.approx_quantiles(hl.len(ht_agg.values), 0.5))
@@ -42,7 +44,7 @@ def query():
         ylabel='ld variants (n)',
         title='Linked variants across the genome',
     )
-    plot_filename_html = output_path(f'ld_variants_across_genome.html', 'web')
+    plot_filename_html = output_path(f'ld_variants_across_genome_biallelic.html', 'web')
     html = file_html(plot, CDN, 'my plot')
     with hl.hadoop_open(plot_filename_html, 'w') as f:
         f.write(html)
