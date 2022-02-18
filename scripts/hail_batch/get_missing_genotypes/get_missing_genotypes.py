26c0069bba8e6045d8c8e6f53e0294e34e62ffd3
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-02-18 15:35:44 +1100

    Get number of NA genotypes before and after filtering

---
+++ scripts/hail_batch/get_missing_genotypes/get_missing_genotypes.py
@@ -0,0 +1,28 @@
+"""Get number of variants with a MAF > 0.05"""
+
+import hail as hl
+from analysis_runner import bucket_path
+
+TOB_WGS = bucket_path('mt/v5.1.mt/')
+
+
+def query():
+    """Query script entry point."""
+
+    hl.init(default_reference='GRCh38')
+
+    tob_wgs = hl.read_matrix_table(TOB_WGS)
+    # densify the table and split multiallelics
+    tob_wgs = hl.experimental.densify(tob_wgs)
+    tob_wgs = hl.split_multi_hts(tob_wgs)
+    # count number of rows before filtering and print
+    nrows_pre = tob_wgs.count_rows()
+    print(f'There are {nrows_pre} rows before filtering')
+    # filter out rows with NA genotypes
+    tob_wgs = tob_wgs.filter_rows(hl.agg.any(hl.is_missing(tob_wgs.GT)))
+    nrows_post = tob_wgs.count_rows()
+    print(f'There are {nrows_post} rows after filtering')
+
+
+if __name__ == '__main__':
+    query()
