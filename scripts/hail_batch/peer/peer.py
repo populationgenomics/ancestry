1c16d526e86acb9e126cb343fcba49b2b33d5b66
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-02-23 14:32:12 +1100

    Convert csv file to json

--- scripts/hail_batch/peer/peer.py
+++ scripts/hail_batch/peer/peer.py
@@ -22,7 +22,10 @@ PEER_DOCKER = "australia-southeast1-docker.pkg.dev/cpg-common/images/peer:1.3"
 
 SCORES_PATH = 'gs://cpg-tob-wgs-test/kat/pca/nfe_feb22/v0/scores.tsv'
 COVARIATES_PATH = 'gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/covariates_files/covariates.tsv'
-SAMPLE_ID_KEYS_PATH = 'gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/OneK1K_CPG_IDs.tsv'
+SAMPLE_ID_KEYS_PATH = (
+    'gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/OneK1K_CPG_IDs.tsv'
+)
+
 
 def get_covariates(scores_path, covariates_path, sample_id_keys_path):
     """
@@ -32,19 +35,24 @@ def get_covariates(scores_path, covariates_path, sample_id_keys_path):
     scores_df = pd.read_csv(scores_path, sep='\t')
     sampleid = scores_df.s
     # only get the first 4 PCs, as indicated by scree plot
-    scores = pd.DataFrame(scores_df['scores'].to_list()).iloc[:,0:4]
-    scores.columns =['PC1', 'PC2', 'PC3', 'PC4']
+    scores = pd.DataFrame(scores_df['scores'].to_list()).iloc[:, 0:4]
+    scores.columns = ['PC1', 'PC2', 'PC3', 'PC4']
     scores.insert(loc=0, column='sampleid', value=sampleid)
 
     # get age and sex from Seyhan's covariate file
     covariates = pd.read_csv(covariates_path, sep='\t')
     # load in keys file to convert sampleids in covariates file to CPG IDs
-    sampleid_keys = pd.read_csv(sample_id_keys_path, sep = '\t')
-    covariates = pd.merge(covariates,sampleid_keys,how='left',left_on='sampleid',right_on='OneK1K_ID').drop(['pc1', 'pc2', 'pc3', 'pc4'], axis=1)
+    sampleid_keys = pd.read_csv(sample_id_keys_path, sep='\t')
+    covariates = pd.merge(
+        covariates, sampleid_keys, how='left', left_on='sampleid', right_on='OneK1K_ID'
+    ).drop(['pc1', 'pc2', 'pc3', 'pc4'], axis=1)
     # merge together covariates df and scores
-    covariates = pd.merge(covariates,scores,how='left',left_on='InternalID',right_on='sampleid').drop(['OneK1K_ID','InternalID','ExternalID','sampleid_y'], axis=1)
+    covariates = pd.merge(
+        covariates, scores, how='left', left_on='InternalID', right_on='sampleid'
+    ).drop(['OneK1K_ID', 'InternalID', 'ExternalID', 'sampleid_y'], axis=1)
 
     return covariates.to_numpy()
+
 
 def run_peer(expression_file, covariates, factors_output_path):
     """
@@ -87,10 +95,15 @@ def run_peer(expression_file, covariates, factors_output_path):
 
     return (precision, weights, residuals, factors)
 
+
 # @click.command()
 # @click.option('--expression-file')
-def main(expression_file, scores_path=SCORES_PATH, covariates_path=COVARIATES_PATH, sample_id_keys_path=SAMPLE_ID_KEYS_PATH):
-
+def main(
+    expression_file,
+    scores_path=SCORES_PATH,
+    covariates_path=COVARIATES_PATH,
+    sample_id_keys_path=SAMPLE_ID_KEYS_PATH,
+):
 
     dataset = os.getenv('DATASET', 'tob-wgs')
     access_level = os.getenv('ACCESS_LEVEL', 'test')
@@ -104,18 +117,23 @@ def main(expression_file, scores_path=SCORES_PATH, covariates_path=COVARIATES_PA
 
     load_covariates = batch.new_python_job('load covariates')
 
-    covariates_np_list = load_covariates.call(get_covariates, scores_path, covariates_path, sample_id_keys_path)
+    covariates_np_list = load_covariates.call(
+        get_covariates, scores_path, covariates_path, sample_id_keys_path
+    )
 
     peer_job = batch.new_python_job('run_peer')
     peer_job.image(PEER_DOCKER)
-    peer_job_intermediates: hb.PythonResult = peer_job.call(run_peer, expression_file=expression_f, covariates=covariates_np_list)
+    peer_job_intermediates: hb.PythonResult = peer_job.call(
+        run_peer, expression_file=expression_f, covariates=covariates_np_list
+    )
 
     second_job = batch.new_python_job('downstream tasks')
     second_job.call(print, peer_job_intermediates)
 
     batch.run(wait=False)
 
+
 if __name__ == "__main__":
     main(
         expression_file='gs://cpg-tob-wgs-test/kat/expression.csv',
-    ) # pylint: disable=no-value-for-parameter
+    )  # pylint: disable=no-value-for-parameter
