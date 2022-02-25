e3f0c6f575c9f3bb6e7d1b275b3be6566e3ae021
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-02-25 13:46:54 +1100

    Run PCA on all NFE samples, with outliers included

---
+++ scripts/hail_batch/final_tob_batch_pca/generate_pca_all_samples.py
@@ -0,0 +1,45 @@
+"""
+Perform pca on nfe samples from the HGDP/1KG +
+tob-wgs dataset without removing any outlier samples.
+Reliant on output from
+```joint-calling/scripts/ancestry_pca.py```
+"""
+
+import hail as hl
+import pandas as pd
+from analysis_runner import output_path
+
+
+HGDP1KG_TOBWGS = (
+    'gs://cpg-tob-wgs-main-analysis/joint-calling/v7/ancestry/mt_union_hgdp.mt/'
+)
+
+
+def query():
+    """Query script entry point."""
+    hl.init(default_reference='GRCh38')
+
+    mt = hl.read_matrix_table(HGDP1KG_TOBWGS)
+    # Get samples from the specified population only
+    mt = mt.filter_cols(
+        (mt.hgdp_1kg_metadata.population_inference.pop == 'nfe')
+        | (mt.s.contains('CPG'))
+    )
+
+    # Remove related samples at the 2nd degree or closer, as indicated by gnomAD
+    mt = mt.filter_cols(mt.hgdp_1kg_metadata.gnomad_release | mt.s.startswith('CPG'))
+
+    # Perform PCA
+    eigenvalues_path = output_path('eigenvalues.ht')
+    scores_path = output_path('scores.ht')
+    loadings_path = output_path('loadings.ht')
+    eigenvalues, scores, loadings = hl.hwe_normalized_pca(
+        mt.GT, compute_loadings=True, k=20
+    )
+    hl.Table.from_pandas(pd.DataFrame(eigenvalues)).export(eigenvalues_path)
+    scores.write(scores_path, overwrite=True)
+    loadings.write(loadings_path, overwrite=True)
+
+
+if __name__ == '__main__':
+    query()
