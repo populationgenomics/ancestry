1431e3f94568fc8c85d0631783b0a168f70d7e0e
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-03-23 10:01:29 +0800

    Test running PCA without bucket_path module

--- scripts/hail_batch/final_tob_batch_pca/generate_pca_no_outliers.py
+++ scripts/hail_batch/final_tob_batch_pca/generate_pca_no_outliers.py
@@ -7,10 +7,13 @@ Reliant on output from
 
 import hail as hl
 import pandas as pd
-from analysis_runner import bucket_path, output_path
+from cpg_utils.hail import output_path
 
 
-HGDP1KG_TOBWGS = bucket_path(f'joint-calling/v7/ancestry/mt_union_hgdp.mt', 'analysis')
+# HGDP1KG_TOBWGS = dataset_path(f'joint-calling/v7/ancestry/mt_union_hgdp.mt', 'analysis')
+HGDP1KG_TOBWGS = (
+    'gs://cpg-tob-wgs-main-analysis/joint-calling/v7/ancestry/mt_union_hgdp.mt/'
+)
 
 
 def query():
